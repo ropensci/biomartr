ffbee21a7f5fa3a0d91e01c4c28ea316fcf8053e
HajkD <hajkdrost@googlemail.com>
2014-12-10 16:31:42 +0100

    adding content to the function getGO();
    updating documentation of biomart();

--- R/biomart.R
+++ R/biomart.R
@@ -1,22 +1,23 @@
 #' @title Main function to query BioMart
 #' @description This function takes a set of gene ids and the biomart specifications
 #' and performs a biomart query for the given set of gene ids.
-#' @param genes a character vector storing the gene ids of a organisms of interest to be queried against biomart.
+#' @param genes a character vector storing the gene ids of a organisms of interest to be queried against BioMart.
 #' @param mart a character string specifying the mart to be used, e.g. mart = "ensembl".
-#' @param dataset a character string specifying the dataset within the mart to be used, e.g. dataset = "hsapiens_gene_ensembl"
-#' @param attributes a character vector specifying the attributes that shall be used, e.g. attributes = c("start_position","end_position","description")
-#' @param filters a character vector specifying the filter (query key) for the biomart query, e.g. filter = "ensembl_gene_id"
-#' @param ... additional parameters for the biomaRt::getBM() function
+#' @param dataset a character string specifying the dataset within the mart to be used, e.g. \code{dataset} = \code{"hsapiens_gene_ensembl"}.
+#' @param attributes a character vector specifying the attributes that shall be used, e.g. \code{attributes} = \code{c("start_position","end_position","description")}.
+#' @param filters a character vector specifying the filter (query key) for the BioMart query, e.g. \code{filter} = \code{"ensembl_gene_id"}.
+#' @param ... additional parameters for the \code{\link[biomaRt]{getBM}} function.
 #' @author Hajk-Georg Drost
-#' @details The biomart() function is the main query function of the biomartr package.
-#' It enables to fastly access annotations of a given gene set based on the biomaRt package
+#' @details This function is the main query function of the biomartr package.
+#' 
+#' It enables to fastly access annotations of a given gene set based on the \pkg{biomaRt} package
 #' implemented by Steffen Durinck et al. 
 #' 
 #' @examples 
 #' # the initial biomaRt workflow would work as follows:
 #' 
 #' # 1) select a mart and data set
-#' mart <- useDataset("athaliana_eg_gene", mart = useMart("plants_mart_22"))
+#' mart <- useDataset("athaliana_eg_gene", mart = useMart("plants_mart_23"))
 #' 
 #' # 2) run a biomart query using the getBM() function
 #' # and specify the attributes and filter arguments
@@ -28,16 +29,18 @@
 #' # for faster query access and easier query logic the
 #' # biomart() function combines this workflow
 #'                      
-#' # using mart: 'plants_mart_22', dataset: "athaliana_eg_gene"
+#' # using mart: 'plants_mart_23', dataset: "athaliana_eg_gene"
 #' # attributes: c("start_position","end_position","description")
 #' # for an example gene set of Arabidopsis thaliana: c("AT1G06090", "AT1G06100", "AT1G06110", "AT1G06120", "AT1G06130", "AT1G06200") 
 #' 
 #' biomart(genes = c("AT1G06090", "AT1G06100", "AT1G06110", "AT1G06120", "AT1G06130", "AT1G06200"),
-#'         mart = "plants_mart_22", dataset = "athaliana_eg_gene",attributes = c("start_position","end_position","description"),
+#'         mart = "plants_mart_23", dataset = "athaliana_eg_gene",attributes = c("start_position","end_position","description"),
 #'         filters = "tair_locus") 
 #' 
-#' @return A data.frame storing the initial query gene vector in the first column, the output 
+#' @return A data.table storing the initial query gene vector in the first column, the output 
 #' gene vector in the second column, and all attributes in the following columns.
+#' @seealso \code{\link{organismFilters}}, \code{\link{organismBM}},
+#' \code{\link[biomaRt]{listAttributes}}, \code{\link[biomaRt]{getBM}}
 #' @export
 biomart <- function(genes, mart, dataset, attributes, filters, ...){
         
@@ -52,7 +55,6 @@ biomart <- function(genes, mart, dataset, attributes, filters, ...){
         colnames(genes) <- filters
         
         tbl_biomart <- merge(query, genes, by = filters)
-        
         
         return(tbl_biomart)
         
--- R/getGO.R
+++ R/getGO.R
@@ -1,20 +1,56 @@
-#' @title Function to retrieve GO terms for a given set of genes
-#' @description This function takes a gene id as character vector and returns the 
-#' corresponding GO term.
-#' @param ...
+#' @title Function to retrieve GO terms for a given set of genes from a query organism
+#' @description This function takes a gene id as character vector from a given query organism and returns the 
+#' corresponding GO terms and additional GO information.
+#' @param organism a character string specifying the scientific name of a query organism.
+#' @param genes a character vector storing the gene ids of a organisms of interest to be queried against BioMart.
+#' @param filters a character vector specifying the filter (query key) for the BioMart query, e.g. \code{filter} = \code{"ensembl_gene_id"}.
+#' @param ... additional parameters for the \code{\link{biomart}} function.
 #' @author Hajk-Georg Drost
-#' @details GO. 
+#' @details 
+#' This function takes the scientific name of a query organism, a set of genes for which GO terms
+#' and additional information shall be retrieved, and a filter argument that specifies the attribute
+#' for the query genes.  
+#' 
 #' @examples \dontrun{
-#' getGO(genes =c("AT1G06090", "AT1G06100", "AT1G06110", "AT1G06120", "AT1G06130", "AT1G06200"),
-#' mart = "plants_mart_22",dataset = "athaliana_eg_gene",
-#' attributes = c("go_accession","go_definition_1006","go_name_1006"),
-#' filters = "tair_locus",uniqueRows = FALSE)
+#' 
+#' GO_tbl <- getGO(organism = "Arabidopsis thaliana", 
+#'                 genes    = c("AT1G06090", "AT1G06100", "AT1G06110", "AT1G06120", "AT1G06130", "AT1G06200"),
+#'                 filters  = "tair_locus")
+#' 
+#' # look at the result
+#' head(GO_tbl[ , c("tair_locus","go_accession","goslim_goa_accession","goslim_goa_description")])
 #' 
 #' }
+#' @seealso \code{\link{biomart}}, \code{\link{organismFilters}}, \code{\link{organismBM}}, \code{\link[biomaRt]{getBM}}
 #' @export
-getGO <- function(...){
+
+getGO <- function(organism, genes, filters, ...){
         
-        return( biomart( ... ) )
+        orgAttr <- organismAttributes(organism = organism, topic = "go")
+        
+        GOattributes <- c("go_accession",
+                          "go_definition_1006",
+                          "go_name_1006",
+                          "goslim_goa_accession",
+                          "goslim_goa_description")
+        
+        GOattr_df <- dplyr::filter(orgAttr, name %in% GOattributes)
+        
+        if(dim(GOattr_df)[1] == 0)
+                stop("Unfortunately for '",organism,"' no GO attributes could be found.")
+        
+        
+        m <- names(table(GOattr_df[ , "mart"]))
+        d <- names(table(GOattr_df[ , "dataset"]))
+        
+        if((length(m) > 1) | (length(d) > 1))
+                stop("GO related attributes have been found in more than one mart or dataset: mart = ",m," ; dataset = ",dataset)
+        
+        return( biomart(genes      = genes,
+                        mart       = m, 
+                        dataset    = d,
+                        attributes = GOattributes,
+                        filters    = filters, ...) )
 }
 
 
