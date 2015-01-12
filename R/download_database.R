defbaa09c6cbb7f913c597608d3091b16bfc092d
HajkD <hajkdrost@googlemail.com>
2015-01-12 21:40:11 +0100

    add new argument 'path' to download_database()

--- R/download_database.R
+++ R/download_database.R
@@ -3,9 +3,12 @@
 #' \code{\link{listDatabases}} to your local hard drive.
 #' @param name a character string specifying the database that shall be downloaded (selected from \code{\link{listDatabases}}).
 #' @param db_format a character string specifying database format, e.g. \code{db_format} = \code{"blastdb"}.
+#' @param path a character string specifying the location (a folder) in which the corresponding
+#' database shall be stored. Default is \code{path} = \code{"DB"}.
 #' @details
 #' This function downloads large databases to your hard drive. For this purpose a folder
-#' named \code{DB} is created and the correspondning database 
+#' named \code{DB} (default) is created and the correspondning database then stored in this folder. 
+#' 
 #' @author Hajk-Georg Drost
 #' @examples 
 #' \dontrun{
@@ -21,15 +24,15 @@
 #' }
 #' @export
 
-download_database <- function(name, db_format = "blastdb"){
+download_database <- function(name, db_format = "blastdb", path = "DB"){
         
         if(!is.element(db_format,c("blastdb","fasta")))
                 stop("db_format = '",db_format,"' is not supported by this function.")
         
-        if(!file.exists("DB"))
-                dir.create("DB")
+        if(!file.exists(path))
+                dir.create(path)
         
-        downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/",name), file.path("DB",name) , mode = "wb")
+        downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/",name), file.path(path,name) , mode = "wb")
         
         # limit NCBI queries
         Sys.sleep(0.33)
