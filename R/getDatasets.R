1b8b4332e7a7b4f20a5e708d7bad47164b01ccb1
HajkD <hajkdrost@googlemail.com>
2015-02-11 11:28:29 +0100

    add a tryCatch statement to getDatasets()

--- R/getDatasets.R
+++ R/getDatasets.R
@@ -1,5 +1,5 @@
 #' @title Retrieve All Available Datasets for a BioMart Database
-#' @description This funcion queries the BioMart Interface and returns a table
+#' @description This funcion queries the BioMart API and returns a table
 #' storing all available datasets for a selected BioMart databases.
 #' 
 #' @param mart a character string specifying the database (mart) for which datasets shall be listed.
@@ -9,9 +9,13 @@
 #' # search for available datasets
 #' head(getMarts(), 10)
 #' 
-#' # choose database: "plants_mart_24"
+#' \dontrun{
+#' 
+#' # choose database: "plants_mart_25" -> these versions vary over time
 #' # and get a table of all available datasets from this BioMart database
-#' head(getDatasets("plants_mart_24"), 10)
+#' head(getDatasets("plants_mart_25"), 10)
+#' 
+#' }
 #' 
 #' @seealso \code{\link{getMarts}}, \code{\link{getAttributes}}, \code{\link{getFilters}}, \code{\link{organismBM}}, \code{\link{organismFilters}}, \code{\link{organismAttributes}}
 #' @export
@@ -23,22 +27,31 @@ getDatasets <- function(mart){
         datasetPage <- httr::handle(paste0("http://www.biomart.org/biomart/martservice?type=datasets&requestid=biomart&mart=",mart))
         xmlContentDatasets <- httr::GET(handle = datasetPage)
         
-        httr::stop_for_status(xmlContentDatasets)
+        tryCatch({
+                
+                httr::stop_for_status(xmlContentDatasets)
         
-        # extract dataset name, description, and version, etc.
-        rawDF <- do.call("rbind",apply(as.data.frame(strsplit(httr::content(xmlContentDatasets,as = "text"),"\n")),1,function(x) unlist(strsplit(x,"\t"))))
+                # extract dataset name, description, and version, etc.
+                rawDF <- do.call("rbind",apply(as.data.frame(strsplit(httr::content(xmlContentDatasets,as = "text"),"\n")),1,function(x) unlist(strsplit(x,"\t"))))
         
-        colnames(rawDF) <- paste0("V",1:ncol(rawDF))
+                colnames(rawDF) <- paste0("V",1:ncol(rawDF))
          
-        if(dim(rawDF)[1] > 2)
-                # store available datasets
-                dsBioMart <- as.data.frame(rawDF[-seq(1,nrow(rawDF),2), c("V2","V3","V5")], stringsAsFactors = FALSE, colClasses = rep("character",3))
+                if(dim(rawDF)[1] > 2)
+                        # store available datasets
+                        dsBioMart <- as.data.frame(rawDF[-seq(1,nrow(rawDF),2), c("V2","V3","V5")], stringsAsFactors = FALSE, colClasses = rep("character",3))
         
-        if(dim(rawDF)[1] <= 2)
-                dsBioMart <- data.frame(V2 = "", V3 = "", V5 = "")
+                if(dim(rawDF)[1] <= 2)
+                        dsBioMart <- data.frame(V2 = "", V3 = "", V5 = "")
         
-        colnames(dsBioMart) <- c("dataset","description","version")
+                colnames(dsBioMart) <- c("dataset","description","version")
         
-        return(dsBioMart)
+                return(dsBioMart)
+        
+        }, error = function(e) stop("Your input mart '",mart,"' could not be found. Please use getMarts() to choose from available marts."))
         
 }
+
+
+
+
+
