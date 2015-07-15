0a59c3db8ffdf0cf5f38e672da42487c0f3a9b4c
HajkD <hajkdrost@googlemail.com>
2015-07-15 22:25:14 +0100

    adding unit test for organismAttributes()

---
+++ tests/testthat/test-organismAttributes.R
@@ -0,0 +1,12 @@
+context("Test: organismAttributes()")
+
+
+equal_lists <- function(df1, df2) {
+        isTRUE(all.equal(df1, df2))
+}
+
+test_that("The organismAttributes() interface works properly..",{
+        
+        oattr <- organismAttributes("Arabidopsis thaliana")
+        expect_identical(as.character(oattr[1, 1]),"3_utr_end")
+})
