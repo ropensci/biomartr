df1ccea3863071311af8518de42efa1c83331427
HajkD <hajkdrost@googlemail.com>
2015-07-15 22:29:57 +0100

    adding unit test for organismFilters()

---
+++ tests/testthat/test-organismFilters.R
@@ -0,0 +1,12 @@
+context("Test: organismFilters()")
+
+
+equal_lists <- function(df1, df2) {
+        isTRUE(all.equal(df1, df2))
+}
+
+test_that("The organismFilters() interface works properly..",{
+        
+        oF <- organismFilters("Arabidopsis thaliana")
+        expect_identical(as.character(oF[1, 1]),"affy_ath1_121501")
+})
