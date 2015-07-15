b5ba43c0017728c7d423946e17718ad7316e81e2
HajkD <hajkdrost@googlemail.com>
2015-07-15 16:22:03 +0100

    adding unit test for getAttributes()

---
+++ tests/testthat/test-getAttributes.R
@@ -0,0 +1,18 @@
+context("Test: getAttributes()")
+
+
+equal_lists <- function(df1, df2) {
+        isTRUE(all.equal(df1, df2))
+}
+
+
+test_that("The getAttributes() interface works properly..",{
+        
+        marts <- getMarts()[ , "mart"]
+        plant_mart <- which(stringr::str_match(marts, "plants_mart_") != "NA")
+        
+        attrib <- getAttributes(mart = as.character(marts[plant_mart]), dataset = "athaliana_eg_gene")
+        
+        expect_true(equal_lists(as.list(attrib[1 , ]), list(name = "ensembl_gene_id",
+                                                            description = "Gene stable ID")))
+})
