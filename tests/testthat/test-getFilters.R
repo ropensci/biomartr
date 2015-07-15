04c89ba8d3425f9f0f25db39fccd3eb100b646ad
HajkD <hajkdrost@googlemail.com>
2015-07-15 18:25:36 +0100

    adding unit test for getFilters()

---
+++ tests/testthat/test-getFilters.R
@@ -0,0 +1,23 @@
+context("Test: getFilters()")
+
+
+equal_lists <- function(df1, df2) {
+        isTRUE(all.equal(df1, df2))
+}
+
+
+test_that("The getFilters() interface works properly..",{
+        
+        marts <- getMarts()[ , "mart"]
+        plant_mart <- which(stringr::str_match(marts, "plants_mart_") != "NA")
+        
+        attrib <- getFilters(mart = as.character(marts[plant_mart]), dataset = "athaliana_eg_gene")
+        
+        expect_true(equal_lists(as.list(attrib[1 , ]), list(name = "chromosome_name",
+                                                            description = "Chromosome name")))
+})
+
+
+
+
+
