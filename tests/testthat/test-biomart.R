5b999e8c01647099161c6076733a47011ae8a269
HajkD <hajkdrost@googlemail.com>
2015-07-15 16:09:10 +0100

    adding unit test for biomart()

---
+++ tests/testthat/test-biomart.R
@@ -0,0 +1,24 @@
+context("Test: biomart()")
+
+equal_lists <- function(df1, df2) {
+        isTRUE(all.equal(df1, df2))
+}
+
+
+test_that("The biomart() interface works properly..",{
+        
+        marts <- getMarts()[ , "mart"]
+        plant_mart <- which(stringr::str_match(marts, "plants_mart_") != "NA")
+        
+        expect_true(equal_lists(as.list(biomart(genes      = c("AT1G06090", "AT1G06100",
+                               "AT1G06110", "AT1G06120",
+                               "AT1G06130", "AT1G06200"),
+                mart       = as.character(marts[plant_mart]),
+                dataset    = "athaliana_eg_gene",
+                attributes = c("start_position","end_position","description"),
+                filters    = "tair_locus")[1, ]), list(tair_locus = "AT1G06090",
+                                                       start_position = 1847883,
+                                                       end_position = 1849693,
+                                                       description = "Fatty acid desaturase family protein [Source:TAIR;Acc:AT1G06090]")))
+        
+})
