148c1a3ceec9afb197552191a50b4b360169c3e4
HajkD <hajkdrost@googlemail.com>
2015-07-23 12:23:07 +0100

    adding unit tests for getCDS()

--- tests/testthat/test-getCDS.R
+++ tests/testthat/test-getCDS.R
@@ -1,15 +1,18 @@
 context("Test: getCDS()")
 
 test_that("The getCDS() interface works properly..",{
+       
+        skip_on_cran()
+        skip_on_travis() 
         
-#         getCDS( db       = "refseq",
-#                 kingdom  = "plant",
-#                 organism = "Arabidopsis thaliana",
-#                 path     = tempdir())
-#         
-#         
-#         file_path <- file.path(tempdir(),"Arabidopsis_thaliana_rna.fna.gz")
-#         Ath_CDS <- read_cds(file_path, format = "fasta")
-#         
-#         expect_identical(Ath_CDS[1,geneids],"NM_001035536.2")
+        getCDS( db       = "refseq",
+                kingdom  = "plant",
+                organism = "Arabidopsis thaliana",
+                path     = tempdir())
+        
+        
+        file_path <- file.path(tempdir(),"Arabidopsis_thaliana_rna.fna.gz")
+        Ath_CDS <- read_cds(file_path, format = "fasta")
+        
+        expect_identical(Ath_CDS[1,geneids],"NM_001035536.2")
 })
