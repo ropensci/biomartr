6f823d7a26c12fd71a1aea175a74ae8631453cf7
HajkD <hajkdrost@googlemail.com>
2015-07-23 12:23:18 +0100

    adding unit test for getGenome()

--- tests/testthat/test-getGenome.R
+++ tests/testthat/test-getGenome.R
@@ -1,15 +1,18 @@
 context("Test: getGenome()")
 
 test_that("The getGenome() interface works properly..",{
+
+        skip_on_cran()
+        skip_on_travis() 
         
-#           getGenome( db       = "refseq",
-#                      kingdom  = "plant",
-#                      organism = "Arabidopsis thaliana",
-#                      path     = tempdir())
-#         
-#         
-#         file_path <- file.path(tempdir(),"Arabidopsis_thaliana_genome.fna.gz")
-#         Ath_Genome <- read_cds(file_path, format = "fasta")
-#         
-#         expect_identical(Ath_Genome[1,geneids],"NC_000932.1")
+          getGenome( db       = "refseq",
+                     kingdom  = "plant",
+                     organism = "Arabidopsis thaliana",
+                     path     = tempdir())
+        
+        
+        file_path <- file.path(tempdir(),"Arabidopsis_thaliana_genome.fna.gz")
+        Ath_Genome <- read_cds(file_path, format = "fasta")
+        
+        expect_identical(Ath_Genome[1,geneids],"NC_000932.1")
 })
