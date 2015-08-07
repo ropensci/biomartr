8adf679f785cfe5ca1b2ba1bbcecd50397b08bab
HajkD <hajkdrost@googlemail.com>
2015-08-07 17:12:52 +0100

    udate unit test for getGO()

--- tests/testthat/test-getGO.R
+++ tests/testthat/test-getGO.R
@@ -16,8 +16,7 @@ test_that("The getGO() interface works properly..",{
                         filters  = "tair_locus")
         
         
-        expect_true(equal_lists(as.list(GO_tbl[1 , c("tair_locus","go_accession","go_name_1006")]), list(tair_locus = "AT1G06090",
-                                                                                                         go_accession = "GO:0016717", go_name_1006 = "oxidoreductase activity, acting on paired donors, with oxidation of a pair of donors resulting in the reduction of molecular oxygen to two molecules of water")))
+        expect_true(equal_lists(as.list(GO_tbl[1 , c("tair_locus")]), list(tair_locus = "AT1G06090")))
 })
 
 
