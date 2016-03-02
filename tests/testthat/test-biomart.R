ffdb50820b24fa51b38565ecb2eb42b4400fb610
HajkD <hajkdrost@googlemail.com>
2016-03-02 14:15:24 +0000

    update unit test for biomart()

--- tests/testthat/test-biomart.R
+++ tests/testthat/test-biomart.R
@@ -10,16 +10,14 @@ test_that("The biomart() interface works properly..",{
         skip_on_cran()
         
         marts <- getMarts()[ , "mart"]
-        plant_mart <- which(stringr::str_match(marts, "plants_mart_") != "NA")
+        ex_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
         
-        expect_true(equal_lists(as.list(biomart(genes      = c("AT1G06090", "AT1G06100",
-                               "AT1G06110", "AT1G06120",
-                               "AT1G06130", "AT1G06200"),
-                mart       = as.character(marts[plant_mart]),
-                dataset    = "athaliana_eg_gene",
+        expect_true(equal_lists(as.list(biomart(genes      = "GUCA2A",
+                mart       = as.character(marts[ex_mart]),
+                dataset    = "hsapiens_gene_ensembl",
                 attributes = c("start_position","end_position","description"),
-                filters    = "tair_locus")[1, 1:3]), list(tair_locus = "AT1G06090",
-                                                       start_position = 1847883,
-                                                       end_position = 1849693)))
+                filters    = "hgnc_symbol")[1, 1:3]), list(hgnc_symbol = "GUCA2A",
+                                                       start_position = 42162691,
+                                                       end_position = 42164718)))
         
 })
