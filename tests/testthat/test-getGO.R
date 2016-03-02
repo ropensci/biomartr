09af478aee44ac87a84a1565b97c23391668f3d3
HajkD <hajkdrost@googlemail.com>
2016-03-02 14:15:40 +0000

    update unit test for getGO()

--- tests/testthat/test-getGO.R
+++ tests/testthat/test-getGO.R
@@ -9,14 +9,11 @@ test_that("The getGO() interface works properly..",{
         
         skip_on_cran()
         
-        GO_tbl <- getGO(organism = "Arabidopsis thaliana",
-                        genes    = c("AT1G06090", "AT1G06100",
-                                     "AT1G06110", "AT1G06120",
-                                     "AT1G06130", "AT1G06200"),
-                        filters  = "tair_locus")
+        GO_tbl <- getGO(organism = "Homo sapiens",
+                        genes    = "GUCA2A",
+                        filters  = "hgnc_symbol")
         
-        
-        expect_true(equal_lists(as.list(GO_tbl[1 , c("tair_locus")]), list(tair_locus = "AT1G06090")))
+        expect_true(identical(GO_tbl[1 , c("hgnc_symbol")], "GUCA2A"))
 })
 
 
