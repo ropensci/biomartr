5de51e1364c38797663a0c9c10c5dc7f1a8efc44
HajkD <hgd23@cam.ac.uk>
2016-10-05 12:11:50 +0100

    removing argument db_format because it was misleading; adding new error handling to check for availablility of user defined database

--- R/download_database.R
+++ R/download_database.R
@@ -2,7 +2,6 @@
 #' @description This function allows you to download a database selected by
 #' \code{\link{listDatabases}} to your local hard drive.
 #' @param name a character string specifying the database that shall be downloaded (selected from \code{\link{listDatabases}}).
-#' @param db_format a character string specifying database format, e.g. \code{db_format} = \code{"blastdb"} or \code{db_format} = \code{"fasta"}.
 #' @param path a character string specifying the location (a folder) in which the corresponding
 #' database shall be stored. Default is \code{path} = \code{"DB"}. In case this folder does not exist yet, it will be created.
 #' @details
@@ -19,29 +18,33 @@
 #'   # select NCBI nr version 27 =  "nr.27.tar.gz"
 #'   # and download it to your hard drive
 #'   # -> please note that large databases take some time for download!
-#'   download_database(name = "nr.27.tar.gz", db_format = "blastdb")
+#'   download_database(name = "nr.27.tar.gz")
 #' 
 #' }
 #' @export
 
-download_database <- function(name, db_format = "blastdb", path = "DB"){
+download_database <- function(name, path = "DB"){
         
-        if (!is.element(db_format,c("blastdb","fasta")))
-                stop("db_format = '",db_format,"' is not supported by this function. Please choose either db_format = 'blastdb' or db_format = 'fasta'.")
-        
-        if (!file.exists(path))
-                dir.create(path)
-        
-        if (db_format == "blastdb"){
-                downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/",name), file.path(path,name) , mode = "wb")
-        }
-        
-        if (db_format == "fasta"){
-                downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/",name), file.path(path,name) , mode = "wb")
-        }
-        
-        # limit NCBI queries
-        Sys.sleep(0.33)
+    if (!file.exists(path))
+        dir.create(path)
+    
+    db.name <- names(table(unlist(sapply(name, function (x) unlist(stringr::str_split(x,"[.]"))[1]))))
+    
+    if (!is.element(db.name, listDatabases("all")))
+        stop(
+            "The specified database '",
+            name,
+            "' could not be found on NCBI.
+            Please use the listDatabases('all') command to retrieve available databases.",
+            call. = FALSE
+        )
+    
+    downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/", name),
+                         file.path(path, name) ,
+                         mode = "wb")
+    
+    # limit NCBI queries
+    Sys.sleep(0.33)
 }
 
 
