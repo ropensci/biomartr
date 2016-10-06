448aa141d3616474dde6852bcb222c62b66fb244
HajkD <hgd23@cam.ac.uk>
2016-10-06 14:20:06 +0100

    update unit test

--- tests/testthat/test-getProteome.R
+++ tests/testthat/test-getProteome.R
@@ -5,14 +5,14 @@ test_that("The getProteome() interface works properly..",{
         skip_on_cran()
         skip_on_travis() 
         
-        getProteome( db       = "refseq",
-                     kingdom  = "plant",
-                     organism = "Arabidopsis thaliana",
-                     path     = tempdir())
+        # test proper download
+        Ath_Proteome <- read_proteome(getProteome( db       = "refseq",
+                                              organism = "Arabidopsis thaliana",
+                                              path     = tempdir()), format = "fasta")
         
+        # test proper use of internal referece files when command is repeated
+        Ath_Proteome <- read_proteome(getProteome( db       = "refseq",
+                                              organism = "Arabidopsis thaliana",
+                                              path     = tempdir()), format = "fasta")
         
-        file_path <- file.path(tempdir(),"Arabidopsis_thaliana_protein.faa.gz")
-        Ath_Proteome <- read_cds(file_path, format = "fasta")
-        
-        expect_identical(Ath_Proteome[1,geneids],"NP_001030613.1")
 })
