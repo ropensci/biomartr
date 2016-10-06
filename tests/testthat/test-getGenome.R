0895438f3cd9fbab1fefb4eb362f13539b0db2ea
HajkD <hgd23@cam.ac.uk>
2016-10-06 14:20:01 +0100

    update unit test

--- tests/testthat/test-getGenome.R
+++ tests/testthat/test-getGenome.R
@@ -5,14 +5,14 @@ test_that("The getGenome() interface works properly..",{
         skip_on_cran()
         skip_on_travis() 
         
-          getGenome( db       = "refseq",
-                     kingdom  = "plant",
-                     organism = "Arabidopsis thaliana",
-                     path     = tempdir())
+        # test proper download
+        Ath_Genome <- read_genome(getGenome( db       = "refseq",
+                                          organism = "Arabidopsis thaliana",
+                                          path     = tempdir()), format = "fasta")
         
+        # test proper use of internal referece files when command is repeated
+        Ath_Genome <- read_genome(getGenome( db       = "refseq",
+                                          organism = "Arabidopsis thaliana",
+                                          path     = tempdir()), format = "fasta")
         
-        file_path <- file.path(tempdir(),"Arabidopsis_thaliana_genomic.fna.gz")
-        Ath_Genome <- read_cds(file_path, format = "fasta")
-        
-        expect_identical(Ath_Genome[1,geneids],"NC_000932.1")
 })
