c93ff8b3e2a38a8a5cb9ba8d5c48993ad385c98e
HajkD <hgd23@cam.ac.uk>
2016-10-07 14:14:24 +0100

    adding unit tests

--- tests/testthat/test-getGenome.R
+++ tests/testthat/test-getGenome.R
@@ -5,7 +5,7 @@ test_that("The getGenome() interface works properly..",{
         skip_on_cran()
         skip_on_travis() 
         
-        # test proper download
+        # test proper download from genbank
         Ath_Genome <- read_genome(getGenome( db       = "refseq",
                                           organism = "Arabidopsis thaliana",
                                           path     = tempdir()), format = "fasta")
@@ -14,5 +14,15 @@ test_that("The getGenome() interface works properly..",{
         Ath_Genome <- read_genome(getGenome( db       = "refseq",
                                           organism = "Arabidopsis thaliana",
                                           path     = tempdir()), format = "fasta")
+        
+        # test proper download from genbank
+        Ath_Genome <- read_genome(getGenome( db       = "genbank",
+                                             organism = "Arabidopsis thaliana",
+                                             path     = tempdir()), format = "fasta")
+        
+        # test proper use of internal referece files when command is repeated
+        Ath_Genome <- read_genome(getGenome( db       = "genbank",
+                                             organism = "Arabidopsis thaliana",
+                                             path     = tempdir()), format = "fasta")
         
 })
