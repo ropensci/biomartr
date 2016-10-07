3d2388d04da762076914cd3a248db8fcafd1f5cf
HajkD <hgd23@cam.ac.uk>
2016-10-07 14:14:34 +0100

    adding unit tests

--- tests/testthat/test-is.genome.available.R
+++ tests/testthat/test-is.genome.available.R
@@ -1,19 +1,19 @@
 context("Test: is.genome.available()")
 
-
-equal_lists <- function(df1, df2) {
-        isTRUE(all.equal(df1, df2))
-}
-
-
 test_that("The is.genome.available() interface works properly..",{
         
         skip_on_cran()
         
-        g <- is.genome.available(organism = "Arabidopsis thaliana", details = TRUE)
+        # test whether interface to 'refseq' works properly
+        g <- is.genome.available(organism = "Arabidopsis thaliana", details = TRUE, db = "refseq")
         expect_identical(as.character(g[1, 1]),"Arabidopsis thaliana")
         
         # test with a second run using locally stored information
         is.genome.available(organism = "Homo sapiens", details = TRUE)
         
+        # test whether interface to 'genbank' works properly
+        is.genome.available(organism = "Homo sapiens", details = TRUE, db = "genbank")
+        
+        # test whether it works properly a second time based on the internal files
+        is.genome.available(organism = "Arabidopsis thaliana", details = TRUE, db = "genbank")
 })
