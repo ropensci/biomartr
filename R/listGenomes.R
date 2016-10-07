e23136e886d966ce1351a3547fd113c387ea1f63
HajkD <hgd23@cam.ac.uk>
2016-10-07 11:22:01 +0100

    rename argument database to db

--- R/listGenomes.R
+++ R/listGenomes.R
@@ -8,8 +8,8 @@
 #' "chrs","organelles","plasmids", and "bio_projects".
 #' @param update a logical value specifying whether or not the available organism table shall be updated from the NCBI server.
 #' Default is \code{update} = \code{FALSE}.
-#' @param database a character string specifying the database for which genome availability shall be checked, 
-#' e.g. \code{database} =  \code{"refseq"} or \code{database} =  \code{"all"}.
+#' @param db a character string specifying the database for which genome availability shall be checked, 
+#' e.g. \code{db} =  \code{"refseq"} or \code{db} =  \code{"all"}.
 #' @author Hajk-Georg Drost
 #' @details Internally this function loads the the overview.txt file from NCBI:
 #' \url{ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/} and creates a directory '_ncbi_downloads' in the \code{temdir()}
@@ -46,11 +46,11 @@
 #' table(ncbi_genomes[ , "subgroup"])
 #' 
 #' # you can also limit your search to the refseq database
-#' head(listGenomes(database = "refseq") , 20)
+#' head(listGenomes(db = "refseq") , 20)
 #' 
-#' head(listGenomes(details=TRUE, database = "refseq") , 5)
+#' head(listGenomes(details=TRUE, db = "refseq") , 5)
 #' 
-#' head(listGenomes(kingdom = "Eukaryota", details = TRUE,database = "refseq") , 5)
+#' head(listGenomes(kingdom = "Eukaryota", details = TRUE,db = "refseq") , 5)
 #' 
 #' # order by file size
 #' library(dplyr)
@@ -64,99 +64,116 @@
 #' 
 #' @references \url{ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt}
 #' @export
-listGenomes <- function(kingdom = "all", details = FALSE, update = FALSE, database = "all"){
+listGenomes <- function(kingdom = "all", details = FALSE, update = FALSE, db = "all"){
         
-        kingdoms <- organism_name <- NULL
+    kingdoms <- organism_name <- NULL
+    
+    if (!is.element(kingdom,
+                    c("all", "Archaea", "Bacteria", "Eukaryota", "Viroids", "Viruses")))
+        stop("Please use a valid kingdom.")
+    
+    if (!is.element(db, c("refseq", "all")))
+        stop("Please specify a database that is supported by this function.")
+    
+    
+    if (!file.exists(file.path(tempdir(), "_ncbi_downloads"))) {
+        dir.create(file.path(tempdir(), "_ncbi_downloads"))
+    }
+    
+    if (!file.exists(file.path(tempdir(), "_ncbi_downloads", "overview.txt"))) {
+        utils::download.file(
+            "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt",
+            file.path(tempdir(), "_ncbi_downloads", "overview.txt"),
+            quiet = TRUE
+        )
         
-        if(!is.element(kingdom,c("all","Archaea", "Bacteria", "Eukaryota", "Viroids", "Viruses")))
-                stop("Please use a valid kingdom.")
+        # NCBI limits requests to three per second
+        Sys.sleep(0.33)
+    }
+    
+    if (update) {
+        utils::download.file(
+            "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt",
+            file.path(tempdir(), "_ncbi_downloads", "overview.txt"),
+            quiet = TRUE
+        )
         
-        if(!is.element(database, c("refseq","all")))
-                stop("Please specify a database that is supported by this function.")
-        
-        
-        if(!file.exists(file.path(tempdir(),"_ncbi_downloads"))){
-                
-                dir.create(file.path(tempdir(),"_ncbi_downloads"))
-                
+        # NCBI limits requests to three per second
+        Sys.sleep(0.33)
+    }
+    
+    col_classes <- vector(mode = "character", length = 9)
+    col_classes <- c(rep("character", 4), rep("numeric", 5))
+    ncbi_overview <-
+        utils::read.csv(
+            file.path(tempdir(), "_ncbi_downloads", "overview.txt"),
+            sep = "\t",
+            header = TRUE,
+            colClasses = col_classes,
+            na.strings = "-"
+        )
+    
+    names(ncbi_overview) <- c(
+        "organism_name",
+        "kingdoms",
+        "group",
+        "subgroup",
+        "file_size_MB",
+        "chrs",
+        "organelles",
+        "plasmids",
+        "bio_projects"
+    )
+    
+    if (db == "refseq") {
+        if (!file.exists(file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"))) {
+            file.create(file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"))
+            
+            utils::write.table(
+                x         = refseqOrganisms(),
+                file      = file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"),
+                sep       = "\n",
+                quote     = FALSE,
+                col.names = FALSE,
+                row.names = FALSE
+            )
         }
         
-        if(!file.exists(file.path(tempdir(),"_ncbi_downloads","overview.txt"))){
-                
-                utils::download.file("ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt",
-                                     file.path(tempdir(),"_ncbi_downloads","overview.txt"), 
-                                     quiet = TRUE)
-                
-                # NCBI limits requests to three per second
-                Sys.sleep(0.33)
+        refseqOrgs <-
+            utils::read.table(
+                file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"),
+                header           = FALSE,
+                sep              = "\n",
+                colClasses       = "character",
+                stringsAsFactors = FALSE
+            )
+        
+        ncbi_overview <-
+            ncbi_overview[stats::na.omit(match(refseqOrgs[, 1]
+                                               , ncbi_overview[, "organism_name"])),]
+    }
+    
+    if (kingdom == "all") {
+        if (details == TRUE) {
+            return(ncbi_overview)
         }
         
-        if(update){
-                
-                utils::download.file("ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt",
-                                    file.path(tempdir(),"_ncbi_downloads","overview.txt"), 
-                                    quiet = TRUE)
-                
-                # NCBI limits requests to three per second
-                Sys.sleep(0.33)
+        if (details == FALSE) {
+            return(ncbi_overview[, "organism_name"])
+        }
+    }
+    
+    if (kingdom != "all") {
+        if (details == TRUE) {
+            return(dplyr::filter(ncbi_overview, kingdoms == kingdom))
         }
         
-        col_classes <- vector(mode = "character",length = 9)
-        col_classes <- c(rep("character",4),rep("numeric",5))
-        ncbi_overview <- utils::read.csv(file.path(tempdir(),"_ncbi_downloads","overview.txt"),
-                                         sep = "\t", header = TRUE,
-                                         colClasses = col_classes,
-                                         na.strings = "-")
-        
-        names(ncbi_overview) <- c("organism_name","kingdoms",
-                                  "group","subgroup","file_size_MB",
-                                  "chrs","organelles","plasmids",
-                                  "bio_projects")
-        
-        
-        if(database == "refseq"){
-                
-                if(!file.exists(file.path(tempdir(),"_ncbi_downloads","refseqOrgs.txt"))){
-                        file.create(file.path(tempdir(),"_ncbi_downloads","refseqOrgs.txt"))
-                        
-                        utils::write.table(x         = refseqOrganisms(), 
-                                           file      = file.path(tempdir(),"_ncbi_downloads","refseqOrgs.txt"), 
-                                           sep       = "\n",
-                                           quote     = FALSE, 
-                                           col.names = FALSE, 
-                                           row.names = FALSE)
-                }
-                
-                refseqOrgs <- utils::read.table(file.path(tempdir(),"_ncbi_downloads","refseqOrgs.txt"), 
-                                                header           = FALSE, 
-                                                sep              = "\n",
-                                                colClasses       = "character", 
-                                                stringsAsFactors = FALSE)
-                
-                ncbi_overview <- ncbi_overview[stats::na.omit(match(refseqOrgs[ , 1]
-                                                       , ncbi_overview[ , "organism_name"])), ]
+        if (details == FALSE) {
+            return(dplyr::select(
+                dplyr::filter(ncbi_overview, kingdoms == kingdom),
+                organism_name
+            ))
         }
-        
-        if(kingdom == "all"){
-                if(details == TRUE){
-                        return(ncbi_overview)
-                }
-                
-                if(details == FALSE){
-                        return(ncbi_overview[ , "organism_name"])
-                }
-        }
-        
-        if(kingdom != "all"){
-                if(details == TRUE){
-                        return( dplyr::filter(ncbi_overview, kingdoms == kingdom) )
-                }
-                
-                if(details == FALSE){
-                        return( dplyr::select(dplyr::filter(ncbi_overview,kingdoms == kingdom),
-                                              organism_name) )
-                }
-        }
-        
+    }
 }
 
