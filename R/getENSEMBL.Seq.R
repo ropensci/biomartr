ddf43fdad5169f199a22418b2a3d014e0acafc3b
HajkD <hgd23@cam.ac.uk>
2016-10-09 20:02:26 +0100

    adding new function getENSEMBL.Seq()

---
+++ R/getENSEMBL.Seq.R
@@ -0,0 +1,46 @@
+getENSEMBL.Seq <- function(organism, type = "dna", id.type = "toplevel", path) {
+    
+    if (!is.element(type, c("dna", "cds", "pep")))
+        stop("Please a 'type' argument supported by this function: 'dna', 'cds', 'pep'.")
+
+    new.organism <- stringr::str_replace(organism," ","_")
+    
+    # test proper API access
+    tryCatch({
+        json.qry.info <-
+            jsonlite::fromJSON(
+                paste0(
+                    "http://rest.ensembl.org/info/assembly/",
+                    new.organism,
+                    "?content-type=application/json"
+                )
+            )
+    }, error = function(e)
+        stop(
+            "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?"
+        ))
+    
+    # construct retrieval query
+    ensembl.qry <-
+        paste0(
+            "ftp://ftp.ensembl.org/pub/current_fasta/",
+            stringr::str_to_lower(new.organism),
+            "/",
+            type,
+            "/",
+            paste0(new.organism,".",json.qry.info$default_coord_system_version,".",type,".",id.type,".fa.gz")
+        )
+    
+    
+    tryCatch({downloader::download(
+        ensembl.qry,
+        destfile = file.path(path, paste0(new.organism,".",json.qry.info$default_coord_system_version,".",type,".",id.type,".fa.gz")),
+        mode = "wb"
+    )}, error = function(e)
+        stop(
+            "The FTP site of ENSEMBL 'ftp://ftp.ensembl.org/pub/' does not seem to work properly. Are you connected to the internet? Is the site 'ftp://ftp.ensembl.org/pub/' or 'http://rest.ensembl.org' currently available?"
+        )
+    )
+    
+    return(file.path(path, paste0(new.organism,".",json.qry.info$default_coord_system_version,".",type,".",id.type,".fa.gz")))
+}
