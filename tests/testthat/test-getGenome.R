70aac00c4af66356f3e344b28b416c48f9f170d1
HajkD <hgd23@cam.ac.uk>
2016-10-10 16:30:21 +0100

    extend unit test

--- tests/testthat/test-getGenome.R
+++ tests/testthat/test-getGenome.R
@@ -25,4 +25,17 @@ test_that("The getGenome() interface works properly..",{
                                              organism = "Arabidopsis thaliana",
                                              path     = tempdir()), format = "fasta")
         
+        # test proper download from ENSEMBL
+        Scerevisiae_Genome <- read_genome(getGenome( db       = "ensembl",
+                                                     organism = "Saccharomyces cerevisiae",
+                                                     path     = tempdir()), format = "fasta")
+        
+})
+
+
+test_that("The getGenome() error messages work properly..",{
+    
+    expect_error(getGenome( db       = "ensembl",
+               organism = "Saccharomyces cerevisi",
+               path     = tempdir()), "Unfortunately organism 'Saccharomyces cerevisi' is not available at ENSEMBL. Please check whether or not the organism name is typed correctly.")
 })
