5251b65c39ba19a797d971bd3b3808b8f3e91da3
HajkD <hgd23@cam.ac.uk>
2016-10-11 16:30:05 +0100

    add docu

---
+++ R/meta.retrieval.all.R
@@ -0,0 +1,23 @@
+#' @title Perform Meta-Genome Retieval of all organisms in all kingdoms of life
+#' @description Download genomes, proteomes, or CDS of individual species of all kingdoms of life.
+#' @param db a character string specifying the database from which the genome shall be retrieved: \code{refseq} or \code{genbank}.
+#' @param type type of sequences that shall be retrieved. Either \code{genome}, \code{proteome}, or \code{CDS}.
+#' @author Hajk-Georg Drost
+#' @details This function aims to perform bulk retrieval of all genomes of species
+#' for all kingdoms of life.
+#' @examples 
+#' \dontrun{
+#' # download all genomes from refseq
+#' meta.retrieval.all(db = "refseq", type = "genome")
+#' 
+#' # download all vertebrate genomes from genbank
+#' meta.retrieval.all(db = "genbank", type = "genome")
+#' }
+#' @export
+
+meta.retrieval.all <- function(db = "refseq", type = "genome") {
+    
+    sapply(getKingdoms(), function(x) meta.retrieval(x, type = type, db = db))
+    
+    
+}
