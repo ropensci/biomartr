ead5e29a60434f9a223726dd1108c73f9cd7c075
HajkD <hgd23@cam.ac.uk>
2016-10-11 11:27:23 +0100

    rename argument 'db_name' to 'db' to be more consistent with other function notation

--- R/download_database.R
+++ R/download_database.R
@@ -1,50 +1,53 @@
 #' @title Download a NCBI Database to Your Local Hard Drive
 #' @description This function allows you to download a database selected by
 #' \code{\link{listDatabases}} to your local hard drive.
-#' @param name a character string specifying the database that shall be downloaded (selected from \code{\link{listDatabases}}).
+#' @param db a character string specifying the database that shall be downloaded (selected from \code{\link{listDatabases}}).
 #' @param path a character string specifying the location (a folder) in which the corresponding
-#' database shall be stored. Default is \code{path} = \code{"DB"}. In case this folder does not exist yet, it will be created.
+#' database shall be stored. Default is \code{path} = \code{"database"}. In case this folder does not exist yet, it will be created.
 #' @details
 #' This function downloads large databases to your hard drive. For this purpose a folder
-#' named \code{DB} (default) is created and the correspondning database then stored in this folder. 
+#' named \code{database} (default) is created and the correspondning database then stored in this folder. 
 #' 
 #' @author Hajk-Georg Drost
 #' @examples 
 #' \dontrun{
 #'   
 #'   # search for available NCBI nr databases
-#'   listDatabases(db_name = "nr")
+#'   listDatabases(db = "nr")
 #'   
 #'   # select NCBI nr version 27 =  "nr.27.tar.gz"
 #'   # and download it to your hard drive
 #'   # -> please note that large databases take some time for download!
-#'   download_database(name = "nr.27.tar.gz")
+#'   download_database(db = "nr.27.tar.gz")
 #' 
 #' }
+#' @seealso \code{\link{download_database_all}}
 #' @export
 
-download_database <- function(name, path = "DB"){
+download_database <- function(db, path = "database"){
         
     if (!file.exists(path))
         dir.create(path)
     
-    db.name <- names(table(unlist(sapply(name, function (x) unlist(stringr::str_split(x,"[.]"))[1]))))
+    db.name <- names(table(unlist(sapply(db, function(x)
+        unlist(stringr::str_split(x, "[.]"))[1]))))
     
     if (!is.element(db.name, listDatabases("all")))
         stop(
             "The specified database '",
-            name,
+            db,
             "' could not be found on NCBI.
             Please use the listDatabases('all') command to retrieve available databases.",
             call. = FALSE
         )
     
-    downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/", name),
-                         file.path(path, name) ,
+    downloader::download(paste0("ftp://ftp.ncbi.nlm.nih.gov/blast/db/", db),
+                         file.path(path, db) ,
                          mode = "wb")
     
     # limit NCBI queries
-    Sys.sleep(0.33)
+    Sys.sleep(0.33
+    )
 }
 
 
