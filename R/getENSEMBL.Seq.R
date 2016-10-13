aadede88cc1f42051985a9faa384820c0e607c27
HajkD <hgd23@cam.ac.uk>
2016-10-13 17:17:09 +0100

    update code formatting

--- R/getENSEMBL.Seq.R
+++ R/getENSEMBL.Seq.R
@@ -2,8 +2,8 @@ getENSEMBL.Seq <- function(organism, type = "dna", id.type = "toplevel", path) {
     
     if (!is.element(type, c("dna", "cds", "pep")))
         stop("Please a 'type' argument supported by this function: 'dna', 'cds', 'pep'.")
-
-    new.organism <- stringr::str_replace(organism," ","_")
+    
+    new.organism <- stringr::str_replace(organism, " ", "_")
     
     # check if organism is available on ENSEMBL
     tryCatch({
@@ -14,8 +14,13 @@ getENSEMBL.Seq <- function(organism, type = "dna", id.type = "toplevel", path) {
             "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?"
         ))
     
-    if (!is.element(stringr::str_to_lower(new.organism),ensembl.available.organisms$species$name))
-        stop("Unfortunately organism '",organism,"' is not available at ENSEMBL. Please check whether or not the organism name is typed correctly.")
+    if (!is.element(stringr::str_to_lower(new.organism),
+                    ensembl.available.organisms$species$name))
+        stop(
+            "Unfortunately organism '",
+            organism,
+            "' is not available at ENSEMBL. Please check whether or not the organism name is typed correctly."
+        )
     
     # test proper API access
     tryCatch({
@@ -40,19 +45,51 @@ getENSEMBL.Seq <- function(organism, type = "dna", id.type = "toplevel", path) {
             "/",
             type,
             "/",
-            paste0(new.organism,".",json.qry.info$default_coord_system_version,".",type,".",id.type,".fa.gz")
+            paste0(
+                new.organism,
+                ".",
+                json.qry.info$default_coord_system_version,
+                ".",
+                type,
+                ".",
+                id.type,
+                ".fa.gz"
+            )
         )
     
     
-    tryCatch({downloader::download(
-        ensembl.qry,
-        destfile = file.path(path, paste0(new.organism,".",json.qry.info$default_coord_system_version,".",type,".",id.type,".fa.gz")),
-        mode = "wb"
-    )}, error = function(e)
+    tryCatch({
+        downloader::download(ensembl.qry,
+                             destfile = file.path(
+                                 path,
+                                 paste0(
+                                     new.organism,
+                                     ".",
+                                     json.qry.info$default_coord_system_version,
+                                     ".",
+                                     type,
+                                     ".",
+                                     id.type,
+                                     ".fa.gz"
+                                 )
+                             ),
+                             mode = "wb")
+    }, error = function(e)
         stop(
             "The FTP site of ENSEMBL 'ftp://ftp.ensembl.org/pub/' does not seem to work properly. Are you connected to the internet? Is the site 'ftp://ftp.ensembl.org/pub/' or 'http://rest.ensembl.org' currently available?"
-        )
-    )
+        ))
     
-    return(file.path(path, paste0(new.organism,".",json.qry.info$default_coord_system_version,".",type,".",id.type,".fa.gz")))
+    return(file.path(
+        path,
+        paste0(
+            new.organism,
+            ".",
+            json.qry.info$default_coord_system_version,
+            ".",
+            type,
+            ".",
+            id.type,
+            ".fa.gz"
+        )
+    ))
 }
