cfd1537d2ecdefe6d99a6d0b1c0f076c01f90774
HajkD <hgd23@cam.ac.uk>
2016-10-13 16:13:13 +0100

    reduce functionality of KingdomAssembly retrieval to new function getKingdomAssembySummary()

--- R/getGenome.R
+++ R/getGenome.R
@@ -56,69 +56,12 @@ getGenome <-
              path = file.path("_ncbi_downloads", "genomes")) {
         
         if (!is.element(db, c("refseq", "genbank","ensembl")))
-            stop ("Please select one of the available data bases: 'refseq' or 'genbank'")
+            stop("Please select one of the available data bases: 'refseq', 'genbank', or 'ensembl'.")
         
         if (is.element(db, c("refseq", "genbank"))) {
-            # if AssemblyFilesAllKingdoms.txt file was already generated/downloaded then use the local version
-            # stored in temp()
-            if (file.exists(file.path(
-                tempdir(),
-                paste0("AssemblyFilesAllKingdoms_", db, ".txt")
-            ))) {
-                suppressWarnings(
-                    AssemblyFilesAllKingdoms <-
-                        readr::read_tsv(
-                            file.path(
-                                tempdir(),
-                                paste0("AssemblyFilesAllKingdoms_", db, ".txt")
-                            ),
-                            comment = "#",
-                            col_names = TRUE,
-                            col_types = readr::cols(
-                                assembly_accession = readr::col_character(),
-                                bioproject = readr::col_character(),
-                                biosample = readr::col_character(),
-                                wgs_master = readr::col_character(),
-                                refseq_category = readr::col_character(),
-                                taxid = readr::col_integer(),
-                                species_taxid = readr::col_integer(),
-                                organism_name = readr::col_character(),
-                                infraspecific_name = readr::col_character(),
-                                isolate = readr::col_character(),
-                                version_status = readr::col_character(),
-                                assembly_level = readr::col_character(),
-                                release_type = readr::col_character(),
-                                genome_rep = readr::col_character(),
-                                seq_rel_date = readr::col_date(),
-                                asm_name = readr::col_character(),
-                                submitter = readr::col_character(),
-                                gbrs_paired_asm = readr::col_character(),
-                                paired_asm_comp = readr::col_character(),
-                                ftp_path = readr::col_character(),
-                                excluded_from_refseq = readr::col_character()
-                            )
-                        )
-                )
-            } else {
-                # otherwise download all assembly_summary.txt files for all kingdoms and store the AssemblyFilesAllKingdoms.txt file locally
-                # retrieve the assembly_summary.txt files for all kingdoms
-                kgdoms <- getKingdoms(db = db)
-                storeAssemblyFiles <- vector("list", length(kgdoms))
-                
-                for (i in seq_along(kgdoms)) {
-                    storeAssemblyFiles[i] <-
-                        list(getSummaryFile(db = db, kingdom = kgdoms[i]))
-                }
-                
-                AssemblyFilesAllKingdoms <-
-                    dplyr::bind_rows(storeAssemblyFiles)
-                
-                readr::write_tsv(
-                    AssemblyFilesAllKingdoms,
-                    file.path(tempdir(), paste0("AssemblyFilesAllKingdoms_", db, ".txt"))
-                )
-            }
-            
+            # get Kingdom Assembly Summary file
+            AssemblyFilesAllKingdoms <- getKingdomAssemblySummary(db = db)
+        
             # test wheter or not genome is available
             is.genome.available(organism = organism, db = db)
             
@@ -217,7 +160,7 @@ getGenome <-
                 )
             }
         }
-        
+    
         if (db == "ensembl") {
             
             # create result folder
