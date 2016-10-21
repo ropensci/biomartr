e762accb78f16e6a5c7e7f789a56c0b72377db15
HajkD <hgd23@cam.ac.uk>
2016-10-21 17:35:43 +0100

    adding internal file handling

--- R/getENSEMBL.Seq.R
+++ R/getENSEMBL.Seq.R
@@ -7,18 +7,54 @@ getENSEMBL.Seq <- function(organism, type = "dna", id.type = "toplevel", path) {
     
     # test if REST API is responding
     is.ensembl.alive()
+    if (file.exists(file.path(tempdir(), "ensembl_summary.txt"))) {
+        suppressWarnings(ensembl.available.organisms <-
+                             readr::read_tsv(
+                                 file.path(tempdir(), "ensembl_summary.txt"),
+                                 col_names = c(
+                                     "division",
+                                     "taxon_id",
+                                     "name",
+                                     "release",
+                                     "display_name",
+                                     "accession",
+                                     "common_name",
+                                     "assembly"
+                                 ),
+                                 col_types = readr::cols(
+                                     division = readr::col_character(),
+                                     taxon_id = readr::col_integer(),
+                                     name = readr::col_character(),
+                                     release = readr::col_integer(),
+                                     display_name = readr::col_character(),
+                                     accession = readr::col_character(),
+                                     common_name = readr::col_character(),
+                                     assembly = readr::col_character()
+                                 ),
+                                 comment = "#"
+                             ))
+    }
     
-    # check if organism is available on ENSEMBL
-    tryCatch({
+    if (!file.exists(file.path(tempdir(), "ensembl_summary.txt"))) {
+        # check if organism is available on ENSEMBL
+        tryCatch({
+            ensembl.available.organisms <-
+                jsonlite::fromJSON("http://rest.ensembl.org/info/species?content-type=application/json")
+        }, error = function(e)
+            stop(
+                "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?", call. = FALSE
+            ))
+        
+        # transform list object returned by 'fromJSON' to tibble
         ensembl.available.organisms <-
-            jsonlite::fromJSON("http://rest.ensembl.org/info/species?content-type=application/json")
-    }, error = function(e)
-        stop(
-            "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?"
-        ))
+            tibble::as_tibble(dplyr::select(ensembl.available.organisms$species, -aliases, -groups))
+        
+        readr::write_tsv(ensembl.available.organisms,
+                         file.path(tempdir(), "ensembl_summary.txt"))
+    }
     
     if (!is.element(stringr::str_to_lower(new.organism),
-                    ensembl.available.organisms$species$name))
+                    ensembl.available.organisms$name))
         stop(
             "Unfortunately organism '",
             organism,
