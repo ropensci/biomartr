c7a23d2856192b082877da846d4f679f62f28194
HajkD <hgd23@cam.ac.uk>
2016-10-23 17:40:23 +0100

    adding new argument obj.type

--- R/read_cds.R
+++ R/read_cds.R
@@ -1,7 +1,9 @@
-#' @title Read the CDS of a given Organism
+#' @title Import CDS as Biostrings or data.table object
 #' @description This function reads an organism specific CDS stored in a defined file format.
 #' @param file a character string specifying the path to the file storing the CDS.
-#' @param format a character string specifying the file format used to store the CDS, e.g. "fasta", "gbk".
+#' @param format a character string specifying the file format used to store the genome, e.g. \code{format = "fasta"} (default) or \code{format = "gbk"}.
+#' @param obj.type a character string specifying the object stype in which the genomic sequence shall be represented. 
+#' Either as \code{obj.type = "Biostrings"} (default) or as \code{obj.type = "data.table"}.
 #' @param delete_corrupt a logical value specifying whether potential CDS sequences that cannot be divided by 3 shall be
 #' be excluded from the the dataset. Default is \code{delete_corrupt = FALSE}.
 #' @param ... additional arguments that are used by the seqinr::read.fasta() function.
@@ -23,69 +25,96 @@
 #' sequence as string in the second column.
 #' @import data.table
 #' @export
-read_cds <- function(file, format, delete_corrupt = FALSE, ...){
+read_cds <- function(file, format = "fasta", obj.type = "Biostrings", delete_corrupt = FALSE, ...){
     
-    if(!is.element(format, c("fasta", "gbk")))
-        stop("Please choose a file format that is supported by this function.")
+    if (!is.element(format, c("fasta", "gbk")))
+        stop("Please choose a file format that is supported by this function.", call. = FALSE)
+    
+    if (!is.element(obj.type, c("Biostrings", "data.table")))
+        stop(
+            "Please specify a valid object type: obj.type = 'Biostrings' (default) or obj.type = 'data.table'.", call. = FALSE
+        )
     
     geneids <- seqs <- NULL
     
-    tryCatch({
-        cds_file <-
-            Biostrings::readBStringSet(filepath = file, format = format, ...)
+    if (obj.type == "Biostrings") { 
+        tryCatch({
+            cds_file <-
+                Biostrings::readBStringSet(filepath = file, format = format, ...)}, error = function(e) {
+                    stop(
+                        paste0(
+                            "File ",
+                            file,
+                            " could not be read properly. \n",
+                            "Please make sure that ",
+                            file,
+                            " contains only CDS sequences and is in ",
+                            format,
+                            " format."
+                        ), call. = FALSE
+                    )
+                })
         
-        cds_names <-
-            as.vector(unlist(sapply(cds_file@ranges@NAMES, function(x) {
-                return(strsplit(x, " ")[[1]][1])
-            })))
-        
-        cds.dt <-
-            data.table::data.table(geneids = cds_names ,
-                                   seqs = tolower(as.character(cds_file)))
-        
-        
-        data.table::setkey(cds.dt, geneids)
-        
-        mod3 <-
-            function(x) {
-                return((nchar(x) %% 3) == 0)
-            }
-        
-        all_triplets <- cds.dt[, mod3(seqs)]
-        
-        if (!all(all_triplets)) {
-            warning(
-                paste0(
-                    "There have been ",
-                    length(which(!all_triplets)),
-                    " genes found that cannot be divided by 3. To delete these sequences please specify the 'delete_corrupt = TRUE' argument."
-                )
-            )
-        }
-    }, error = function(e) {
-        stop(
-            paste0(
-                "File ",
-                file,
-                " could not be read properly. \n",
-                "Please make sure that ",
-                file,
-                " contains only CDS sequences and is in ",
-                format,
-                " format."
-            )
-        )
-    })
-    
-    if (!all(all_triplets)) {
-        if (delete_corrupt)
-            return(cds.dt[-which(!all_triplets) , list(geneids, seqs)])
-        
-        if (!delete_corrupt)
-            return(cds.dt)
-        
-    } else {
-        return(cds.dt)
+        return(cds_file)
     }
     
+    if (obj.type == "data.table") {
+        tryCatch({
+            cds_file <-
+                Biostrings::readBStringSet(filepath = file, format = format, ...)
+            
+            cds_names <-
+                as.vector(unlist(sapply(cds_file@ranges@NAMES, function(x) {
+                    return(strsplit(x, " ")[[1]][1])
+                })))
+            
+            cds.dt <-
+                data.table::data.table(geneids = cds_names ,
+                                       seqs = tolower(as.character(cds_file)))
+            
+            
+            data.table::setkey(cds.dt, geneids)
+            
+            mod3 <-
+                function(x) {
+                    return((nchar(x) %% 3) == 0)
+                }
+            
+            all_triplets <- cds.dt[, mod3(seqs)]
+            
+            if (!all(all_triplets)) {
+                warning(
+                    paste0(
+                        "There have been ",
+                        length(which(!all_triplets)),
+                        " genes found that cannot be divided by 3. To delete these sequences please specify the 'delete_corrupt = TRUE' argument."
+                    )
+                )
+            }
+        }, error = function(e) {
+            stop(
+                paste0(
+                    "File ",
+                    file,
+                    " could not be read properly. \n",
+                    "Please make sure that ",
+                    file,
+                    " contains only CDS sequences and is in ",
+                    format,
+                    " format."
+                ), call. = FALSE
+            )
+        })
+        
+        if (!all(all_triplets)) {
+            if (delete_corrupt)
+                return(cds.dt[-which(!all_triplets) , list(geneids, seqs)])
+            
+            if (!delete_corrupt)
+                return(cds.dt)
+            
+        } else {
+            return(cds.dt)
+        }
+    }
 }
