4b5ca0069623579281da6dc2e066dfadbdce2c46
HajkD <hgd23@cam.ac.uk>
2016-10-23 12:24:07 +0100

    adding proteome retrieval from ENSEMBL and ENSEMBLGENOMES functionality

--- R/getProteome.R
+++ R/getProteome.R
@@ -1,7 +1,7 @@
 #' @title Proteome Retrieval
 #' @description This function retrieves a fasta-file storing the proteome of an organism of interest and stores
 #' the proteome file in the folder '_ncbi_downloads/proteomes'.
-#' @param db a character string specifying the database from which the genome shall be retrieved: \code{db = "refseq"}, \code{db = "genbank"}, or \code{db = "ensembl"}.
+#' @param db a character string specifying the database from which the genome shall be retrieved: \code{db = "refseq"}, \code{db = "genbank"}, \code{db = "ensembl"} or \code{db = "ensemblgenomes"}.
 #' @param organism a character string specifying the scientific name of the organism of interest, e.g. \code{organism = "Homo sapiens"}.
 #' @param path a character string specifying the location (a folder) in which the corresponding
 #' proteome shall be stored. Default is \code{path} = \code{file.path("_ncbi_downloads","proteomes")}.
@@ -14,9 +14,6 @@
 #' 
 #' and creates a directory '_ncbi_downloads/proteomes' to store
 #' the proteome of interest as fasta file for future processing.
-#' In case the corresponding fasta file already exists within the
-#' '_ncbi_downloads/genomes' folder and is accessible within the workspace,
-#' no download process will be performed.
 #' @return File path to downloaded proteome.
 #' @examples \dontrun{
 #' 
@@ -49,88 +46,176 @@
 
 getProteome <- function(db = "refseq", organism, path = file.path("_ncbi_downloads","proteomes")){
     
-    if (!is.element(db, c("refseq", "genbank", "ensembl")))
-        stop ("Please select one of the available data bases: 'refseq', 'genbank', or 'ensembl'.")
+    if (!is.element(db, c("refseq", "genbank", "ensembl", "ensemblgenomes")))
+        stop("Please select one of the available data bases: 'refseq', 'genbank', 'ensembl' or 'ensemblgenomes'.", call. = FALSE)
     
-    # get Kingdom Assembly Summary file
-    AssemblyFilesAllKingdoms <- getKingdomAssemblySummary(db = db)
-    
-    # test wheter or not genome is available
-    is.genome.available(organism = organism, db = db)
-    
-    if (!file.exists(path)) {
-        dir.create(path, recursive = TRUE)
-    }
-    
-    organism_name <- refseq_category <- version_status <- NULL
-
-    FoundOrganism <-
-        dplyr::filter(
-            AssemblyFilesAllKingdoms,
-            stringr::str_detect(organism_name, organism),
-            ((refseq_category == "representative genome") ||
-                 (refseq_category == "reference genome")
-            ),
-            (version_status == "latest")
-        )
-    
-    if (nrow(FoundOrganism) > 1) {
-        warnings(
-            "More than one entry has been found for '",
-            organism,
-            "'. Only the first entry '",
-            FoundOrganism[1, 1],
-            "' has been used for subsequent proteome retrieval."
-        )
-        FoundOrganism <- FoundOrganism[1, ]
-    }
-    
-    organism <- stringr::str_replace(organism, " ", "_")
-    
-    download_url <-
-        paste0(
-            FoundOrganism$ftp_path,
-            "/",
-            paste0(
-                FoundOrganism$assembly_accession,
-                "_",
-                FoundOrganism$asm_name,
-                "_protein.faa.gz"
+    if (is.element(db, c("refseq", "genbank"))) {
+        # get Kingdom Assembly Summary file
+        AssemblyFilesAllKingdoms <- getKingdomAssemblySummary(db = db)
+        
+        # test wheter or not genome is available
+        is.genome.available(organism = organism, db = db)
+        
+        if (!file.exists(path)) {
+            dir.create(path, recursive = TRUE)
+        }
+        
+        organism_name <- refseq_category <- version_status <- NULL
+        
+        FoundOrganism <-
+            dplyr::filter(
+                AssemblyFilesAllKingdoms,
+                stringr::str_detect(organism_name, organism),
+                ((refseq_category == "representative genome") ||
+                     (refseq_category == "reference genome")
+                ),
+                (version_status == "latest")
             )
-        )
-    
-    if (nrow(FoundOrganism) == 1) {
-        tryCatch({utils::capture.output(downloader::download(
-            download_url,
-            destfile = file.path(path,paste0(organism,"_protein_",db,".faa.gz")),
-            mode = "wb"
-        ))}, error = function(e)
+        
+        if (nrow(FoundOrganism) > 1) {
+            warnings(
+                "More than one entry has been found for '",
+                organism,
+                "'. Only the first entry '",
+                FoundOrganism[1, 1],
+                "' has been used for subsequent proteome retrieval."
+            )
+            FoundOrganism <- FoundOrganism[1,]
+        }
+        
+        organism <- stringr::str_replace(organism, " ", "_")
+        
+        download_url <-
+            paste0(
+                FoundOrganism$ftp_path,
+                "/",
+                paste0(
+                    FoundOrganism$assembly_accession,
+                    "_",
+                    FoundOrganism$asm_name,
+                    "_protein.faa.gz"
+                )
+            )
+        
+        if (nrow(FoundOrganism) == 1) {
+            tryCatch({
+                utils::capture.output(downloader::download(
+                    download_url,
+                    destfile = file.path(path, paste0(
+                        organism, "_protein_", db, ".faa.gz"
+                    )),
+                    mode = "wb"
+                ))
+            }, error = function(e)
+                stop(
+                    "The FTP site 'ftp://ftp.ncbi.nlm.nih.gov/' cannot be reached. Are you connected to the internet? Is the the FTP site '",
+                    download_url,
+                    "' currently available?",
+                    call. = FALSE
+                ))
+            
+            docFile(
+                file.name = paste0(organism, "_protein.faa.gz"),
+                organism  = organism,
+                url       = download_url,
+                database  = db,
+                path      = path,
+                refseq_category = FoundOrganism$refseq_category,
+                assembly_accession = FoundOrganism$assembly_accession,
+                bioproject = FoundOrganism$bioproject,
+                biosample = FoundOrganism$biosample,
+                taxid = FoundOrganism$taxid,
+                infraspecific_name = FoundOrganism$infraspecific_name,
+                version_status = FoundOrganism$version_status,
+                release_type = FoundOrganism$release_type,
+                genome_rep = FoundOrganism$genome_rep,
+                seq_rel_date = FoundOrganism$seq_rel_date,
+                submitter = FoundOrganism$submitter
+            )
+            
+            # NCBI limits requests to three per second
+            Sys.sleep(0.33)
+            
+            
+            print(
+                paste0(
+                    "The proteome of '",
+                    organism,
+                    "' has been downloaded to '",
+                    path,
+                    "' and has been named '",
+                    paste0(organism, "_protein_", db, ".faa.gz"),
+                    "' ."
+                )
+            )
+            
+            return(file.path(path, paste0(
+                organism, "_protein_", db, ".faa.gz"
+            )))
+        } else {
             stop(
-                "The FTP site 'ftp://ftp.ncbi.nlm.nih.gov/' cannot be reached. Are you connected to the internet? Is the the FTP site '",download_url,"' currently available?", call. = FALSE
+                "File: ",
+                download_url,
+                " could not be loaded properly... Are you connected to the internet?", call. = FALSE
+            )
+        }
+    }
+    
+    if (db == "ensembl") {
+        
+        # create result folder
+        if (!file.exists(path)) {
+            dir.create(path, recursive = TRUE)
+        }
+        
+        # download proteome sequence from ENSEMBL
+        proteome.path <- getENSEMBL.Seq(organism, type = "pep", id.type = "all", path)
+        
+        new.organism <- stringr::str_replace(organism," ","_")
+        
+        # test proper API access
+        tryCatch({
+            json.qry.info <-
+                jsonlite::fromJSON(
+                    paste0(
+                        "http://rest.ensembl.org/info/assembly/",
+                        new.organism,
+                        "?content-type=application/json"
+                    )
+                )
+        }, error = function(e)
+            stop(
+                "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?", call. = FALSE
             ))
         
-        docFile(
-            file.name = paste0(organism,"_protein.faa.gz"),
-            organism  = organism,
-            url       = download_url,
-            database  = db,
-            path      = path,
-            refseq_category = FoundOrganism$refseq_category,
-            assembly_accession = FoundOrganism$assembly_accession,
-            bioproject = FoundOrganism$bioproject,
-            biosample = FoundOrganism$biosample,
-            taxid = FoundOrganism$taxid,
-            infraspecific_name = FoundOrganism$infraspecific_name,
-            version_status = FoundOrganism$version_status,
-            release_type = FoundOrganism$release_type,
-            genome_rep = FoundOrganism$genome_rep,
-            seq_rel_date = FoundOrganism$seq_rel_date,
-            submitter = FoundOrganism$submitter
-        )
+        cwd <- getwd()
         
-        # NCBI limits requests to three per second
-        Sys.sleep(0.33)
+        setwd(path)
         
+        # generate Proteome documentation
+        sink(paste0("doc_",new.organism,"_db_",db,".txt"))
+        
+        cat(paste0("File Name: ", proteome.path))
+        cat("\n")
+        cat(paste0("Organism Name: ", new.organism))
+        cat("\n")
+        cat(paste0("Database: ", db))
+        cat("\n")
+        cat(paste0("Download_Date: ", date()))
+        cat("\n")
+        cat(paste0("assembly_name: ", json.qry.info$assembly_name))
+        cat("\n")
+        cat(paste0("assembly_date: ", json.qry.info$assembly_date))
+        cat("\n")
+        cat(paste0("genebuild_last_geneset_update: ", json.qry.info$genebuild_last_geneset_update))
+        cat("\n")
+        cat(paste0("assembly_accession: ", json.qry.info$assembly_accession))
+        cat("\n")
+        cat(paste0("genebuild_initial_release_date: ", json.qry.info$genebuild_initial_release_date))
+        
+        sink()
+        
+        setwd(cwd)
         
         print(
             paste0(
@@ -139,15 +224,85 @@ getProteome <- function(db = "refseq", organism, path = file.path("_ncbi_downloa
                 "' has been downloaded to '",
                 path,
                 "' and has been named '",
-                paste0(organism,"_protein_",db,".faa.gz"),
-                "' ."
+                basename(proteome.path),
+                "'."
             )
         )
         
-        return(file.path(path,paste0(organism,"_protein_",db,".faa.gz")))
-    } else {
-        stop("File: ", download_url, " could not be loaded properly... Are you connected to the internet?")
+        return(proteome.path)
     }
+    
+    if (db == "ensemblgenomes") {
+        
+        # create result folder
+        if (!file.exists(path)) {
+            dir.create(path, recursive = TRUE)
+        }
+        
+        # download proteome sequence from ENSEMBLGENOMES
+        proteome.path <- getENSEMBLGENOMES.Seq(organism, type = "pep", id.type = "all", path)
+        
+        new.organism <- stringr::str_replace(organism," ","_")
+        
+        # test proper API access
+        tryCatch({
+            json.qry.info <-
+                jsonlite::fromJSON(
+                    paste0(
+                        "http://rest.ensemblgenomes.org/info/assembly/",
+                        new.organism,
+                        "?content-type=application/json"
+                    )
+                )
+        }, error = function(e)
+            stop(
+                "The API 'http://rest.ensemblgenomes.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensemblgenomes.org' currently available?", call. = FALSE
+            ))
+        
+        cwd <- getwd()
+        
+        setwd(path)
+        
+        # generate Proteome documentation
+        sink(paste0("doc_",new.organism,"_db_",db,".txt"))
+        
+        cat(paste0("File Name: ", proteome.path))
+        cat("\n")
+        cat(paste0("Organism Name: ", new.organism))
+        cat("\n")
+        cat(paste0("Database: ", db))
+        cat("\n")
+        cat(paste0("Download_Date: ", date()))
+        cat("\n")
+        cat(paste0("assembly_name: ", json.qry.info$assembly_name))
+        cat("\n")
+        cat(paste0("assembly_date: ", json.qry.info$assembly_date))
+        cat("\n")
+        cat(paste0("genebuild_last_geneset_update: ", json.qry.info$genebuild_last_geneset_update))
+        cat("\n")
+        cat(paste0("assembly_accession: ", json.qry.info$assembly_accession))
+        cat("\n")
+        cat(paste0("genebuild_initial_release_date: ", json.qry.info$genebuild_initial_release_date))
+        
+        sink()
+        
+        setwd(cwd)
+        
+        print(
+            paste0(
+                "The proteome of '",
+                organism,
+                "' has been downloaded to '",
+                path,
+                "' and has been named '",
+                basename(proteome.path),
+                "'."
+            )
+        )
+        
+        return(proteome.path)
+    }
+    
 }
 
 
