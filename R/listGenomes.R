932bce0c2ca33a02d317f2e1e0afd9afc8eb3e4b
HajkD <hgd23@cam.ac.uk>
2016-10-27 18:13:03 +0100

    completely rewrite listGenomes() and enable listing foe ENSEMBL and ENSEMBLGENOMES

--- R/listGenomes.R
+++ R/listGenomes.R
@@ -1,179 +1,258 @@
 #' @title List All Available Genomes
 #' @description This function retrieves the names of all genomes available on the NCBI ftp:// server and stores
 #' the results in a file named 'overview.txt' inside the directory '_ncbi_downloads' that
-#' is built inside the workspace. 
-#' @param kingdom a character string specifying a potential filter of available genomes: "all","Archaea", "Bacteria", "Eukaryota", "Viroids", "Viruses".
-#' @param details a boolean value specifying whether only the scientific names of stored genomes shall be returned
-#' (details = FALSE) or all information such as "organism_name","kingdoms", "group","subgroup","file_size_MB",
-#' "chrs","organelles","plasmids", and "bio_projects".
-#' @param update a logical value specifying whether or not the available organism table shall be updated from the NCBI server.
-#' Default is \code{update} = \code{FALSE}.
+#' is built inside the workspace.
 #' @param db a character string specifying the database for which genome availability shall be checked, 
-#' e.g. \code{db} =  \code{"refseq"} or \code{db} =  \code{"all"}.
+#' e.g. \code{db = "refseq"}, \code{db = "genbank"}, \code{db = "ensembl"}, \code{db = "ensemblgenomes"}. 
+#' @param type a character string specifying a potential filter of available genomes. Options are \code{type = "all"}, \code{type = "kingdom"}, \code{type = "group"}, \code{type = "subgroup"}.
+#' @param subset a character string or character vector specifying a subset of \code{type}. E.g. if users are interested in retrieving all
+#' \code{Eukaryota} species, they can specify: \code{type = "kingdom"} and \code{subset = "Eukaryota"}. 
+#' @param details a boolean value specifying whether only the scientific names of stored genomes shall be returned
+#' (details = FALSE) or all information such as \code{organism_name},\code{kingdoms}, \code{group}, \code{subgroup}, \code{file_size_MB}, etc.
 #' @author Hajk-Georg Drost
 #' @details Internally this function loads the the overview.txt file from NCBI:
 #' \url{ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/} and creates a directory '_ncbi_downloads' in the \code{temdir()}
 #' folder to store the overview.txt file for future processing. In case the overview.txt file already exists within the
 #' '_ncbi_downloads' folder and is accessible within the workspace, no download process will be performed again.
-#' @return A data.frame storing either the organism names (details = FALSE)
-#' or all information present on the NCBI database (details = TRUE).
 #' @note
 #' 
-#' Please note that the ftp:// connection relies on the NCBI server and cannot be
+#' Please note that the ftp:// connection relies on the NCBI or ENSEMBL server and cannot be
 #' accurately accessed via a proxy. 
 #' 
 #' @examples \dontrun{
 #' 
-#' # the simplest way to retrieve all names of genomes stored within NCBI databases
-#' head(listGenomes() , 5)
-#' 
-#' # show all details
-#' head(listGenomes(details = TRUE) , 5)
-#' 
-#' # show all details only for Bacteria
-#' head(listGenomes(kingdom = "Bacteria", details = TRUE) , 5)
-#' 
-#' # in case you are interested in the number of genomes available for each kingdom, run:
-#' 
-#' ncbi_genomes <- listGenomes(details = TRUE)
-#' table(ncbi_genomes[ , "kingdoms"])
-#' 
-#' # analogous, if you are interested in the number of genomes available for each group, run:
-#' ncbi_genomes <- listGenomes(details = TRUE)
-#' table(ncbi_genomes[ , "group"])
-#' 
-#' # for subgroup
-#' table(ncbi_genomes[ , "subgroup"])
-#' 
-#' # you can also limit your search to the refseq database
-#' head(listGenomes(db = "refseq") , 20)
-#' 
-#' head(listGenomes(details=TRUE, db = "refseq") , 5)
-#' 
-#' head(listGenomes(kingdom = "Eukaryota", details = TRUE,db = "refseq") , 5)
-#' 
-#' # order by file size
-#' library(dplyr)
-#' head(arrange(ncbi_genomes, desc(file_size_MB)) , 5)
-#' 
-#' 
-#' # you can also update the organism table using the 'update' argument
-#' head(listGenomes(details = TRUE,update = TRUE) , 5)
+#'
 #' 
 #' }
 #' 
 #' @references \url{ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt}
 #' @export
-listGenomes <- function(kingdom = "all", details = FALSE, update = FALSE, db = "all"){
-        
-    kingdoms <- organism_name <- NULL
-    
-    if (!is.element(kingdom,
-                    c("all", "Archaea", "Bacteria", "Eukaryota", "Viroids", "Viruses")))
-        stop("Please use a valid kingdom.")
-    
-    if (!is.element(db, c("refseq", "all")))
-        stop("Please specify a database that is supported by this function.")
-    
-    
-    if (!file.exists(file.path(tempdir(), "_ncbi_downloads"))) {
-        dir.create(file.path(tempdir(), "_ncbi_downloads"))
-    }
 
-    if (!file.exists(file.path(tempdir(), "_ncbi_downloads", "overview.txt"))) {
-        utils::download.file(
-            "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt",
-            file.path(tempdir(), "_ncbi_downloads", "overview.txt"),
-            quiet = TRUE
-        )
+listGenomes <- function(db = "refseq", type = "all", subset = NULL, details = FALSE){
         
-        # NCBI limits requests to three per second
-        Sys.sleep(0.33)
-    }
-    
-    if (update) {
-        utils::download.file(
-            "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/overview.txt",
-            file.path(tempdir(), "_ncbi_downloads", "overview.txt"),
-            quiet = TRUE
+    if (!is.element(db, c("refseq", "genbank", "ensembl", "ensemblgenomes")))
+        stop(
+            "Please specify a database that is supported by this function. E.g. 'refseq', 'genbank', 'ensembl' or 'ensemblgenomes'.",
+            call. = FALSE
         )
+    
+    if (!is.element(type, c("all", "kingdom", "group", "subgroup")))
+        stop(
+            "Please specify a type that is supported by this function. E.g. 'all', kingdom', 'group', or 'subgroup'.",
+            call. = FALSE
+        )
+    
+    if (is.element(db, c("refseq", "genbank"))) {
+        # retrieve genome report overview file
+        ncbi_overview <- getGENOMEREPORT()
         
-        # NCBI limits requests to three per second
-        Sys.sleep(0.33)
-    }
-    
-    col_classes <- vector(mode = "character", length = 9)
-    col_classes <- c(rep("character", 4), rep("numeric", 5))
-    ncbi_overview <-
-        utils::read.csv(
-            file.path(tempdir(), "_ncbi_downloads", "overview.txt"),
-            sep = "\t",
-            header = TRUE,
-            colClasses = col_classes,
-            na.strings = "-"
-        )
-    
-    names(ncbi_overview) <- c(
-        "organism_name",
-        "kingdoms",
-        "group",
-        "subgroup",
-        "file_size_MB",
-        "chrs",
-        "organelles",
-        "plasmids",
-        "bio_projects"
-    )
-    
-    if (db == "refseq") {
-        if (!file.exists(file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"))) {
-            file.create(file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"))
+        # get Kingdom Assembly Summary file
+        AssemblyFilesAllKingdoms <-
+            getKingdomAssemblySummary(db = db)
+        
+        # join tables to retrieve kingdom, group, subgroup information for refseq/genbank organisms
+        joined.df <-
+            dplyr::inner_join(AssemblyFilesAllKingdoms, ncbi_overview, by = "organism_name")
+        
+        kingdoms <- group <-  NULL
+        
+        if (type == "all") {
+            if (details) {
+                if (!is.null(subset)) {
+                    warning(
+                        "For option type = 'all' no subset can be specified. Please select another type and then specify subset = '",
+                        subset,
+                        "'.",
+                        call. = FALSE
+                    )
+                }
+                
+                return(joined.df)
+            }
             
-            utils::write.table(
-                x         = refseqOrganisms(),
-                file      = file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"),
-                sep       = "\n",
-                quote     = FALSE,
-                col.names = FALSE,
-                row.names = FALSE
-            )
+            if (!details) {
+                if (!is.null(subset)) {
+                    warning(
+                        "For option type = 'all' no subset can be specified. Please select another type and then specify subset = '",
+                        subset,
+                        "'.",
+                        call. = FALSE
+                    )
+                }
+                
+                return(unique(joined.df$organism_name))
+            }
         }
         
-        refseqOrgs <-
-            utils::read.table(
-                file.path(tempdir(), "_ncbi_downloads", "refseqOrgs.txt"),
-                header           = FALSE,
-                sep              = "\n",
-                colClasses       = "character",
-                stringsAsFactors = FALSE
-            )
+        if (type == "kingdom") {
+            if (details) {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(
+                        table(joined.df$kingdoms)
+                    ))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'Eukaryota' instead of 'eukaryota'.",
+                            call. = FALSE
+                        )
+                    
+                    return(dplyr::filter(joined.df, is.element(kingdoms, subset)))
+                } else {
+                    return(joined.df)
+                }
+            }
+            
+            if (!details) {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(
+                        table(joined.df$kingdoms)
+                    ))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'Eukaryota' instead of 'eukaryota'.",
+                            call. = FALSE
+                        )
+                    return(unique(
+                        dplyr::filter(joined.df, is.element(kingdoms, subset))$organism_name
+                    ))
+                } else {
+                    return(unique(joined.df$organism_name))
+                }
+            }
+        }
         
-        ncbi_overview <-
-            ncbi_overview[stats::na.omit(match(refseqOrgs[, 1]
-                                               , ncbi_overview[, "organism_name"])),]
+        if (type == "group") {
+            if (details) {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(table(
+                        joined.df$group
+                    )))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'Eukaryota' instead of 'eukaryota'.",
+                            call. = FALSE
+                        )
+                    return(dplyr::filter(joined.df, is.element(group, subset)))
+                } else {
+                    return(joined.df)
+                }
+            }
+            
+            if (!details) {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(table(
+                        joined.df$group
+                    )))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'Eukaryota' instead of 'eukaryota'.",
+                            call. = FALSE
+                        )
+                    return(unique(
+                        dplyr::filter(joined.df, is.element(group, subset))$organism_name
+                    ))
+                } else {
+                    return(unique(joined.df$organism_name))
+                }
+            }
+        }
+        
+        if (type == "subgroup") {
+            if (details) {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(
+                        table(joined.df$subgroup)
+                    ))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'Eukaryota' instead of 'eukaryota'.",
+                            call. = FALSE
+                        )
+                    return(dplyr::filter(joined.df, is.element(subgroup, subset)))
+                } else {
+                    return(joined.df)
+                }
+            }
+            
+            if (!details) {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(
+                        table(joined.df$subgroup)
+                    ))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'Eukaryota' instead of 'eukaryota'.",
+                            call. = FALSE
+                        )
+                    return(unique(
+                        dplyr::filter(joined.df, is.element(subgroup, subset))$organism_name
+                    ))
+                } else {
+                    return(unique(joined.df$organism_name))
+                }
+            }
+        }
     }
     
-    if (kingdom == "all") {
-        if (details == TRUE) {
-            return(ncbi_overview)
-        }
+    if (db == "ensembl") {
+        if (!is.element(type, c("all","kingdom")))
+            stop("Unfortunately, ENSEMBL only provides kingdom information and no group or subgroup information.")
+    
+        ensemblinfo <- get.ensembl.info()
+        ensemblgenomesinfo <-  get.ensemblgenome.info()
         
-        if (details == FALSE) {
-            return(ncbi_overview[, "organism_name"])
+        joined.df <-
+            dplyr::inner_join(ensemblinfo, ensemblgenomesinfo, by = "name")
+        
+        if (details) {
+            if (type == "all")
+                return(joined.df)
+            if (type == "kingdom") {
+                if (!is.null(subset)) {
+                    if (!all(is.element(subset, names(
+                        table(joined.df$division.y)
+                    ))))
+                        stop(
+                            "Unfortunately, not all memebrs of your specified subset '",
+                            paste0(subset, ", '"),
+                            " could be found. Search terms are case sensitive, so you could try to type 'EnsemblMetazoa' instead of 'ensemblmetazoa'.",
+                            call. = FALSE
+                        )
+                    return(dplyr::filter(joined.df, is.element(division.y, subset)))
+                    
+                } else {
+                    return(joined.df)
+                }
+            }
         }
+
+        if (!details) {
+            
+        }
+            return(unique(joined.df$name))
     }
     
-    if (kingdom != "all") {
-        if (details == TRUE) {
-            return(dplyr::filter(ncbi_overview, kingdoms == kingdom))
-        }
+    if (db == "ensemblgenomes") {
         
-        if (details == FALSE) {
-            return(dplyr::select(
-                dplyr::filter(ncbi_overview, kingdoms == kingdom),
-                organism_name
-            ))
-        }
+        ensemblinfo <- get.ensembl.info()
+        
+        
+        
+        
     }
 }
+        
+        
+    
+    
+    
+    
+    
+    
 
