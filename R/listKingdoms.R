8939d2a94a6847a3b4dd0463248ee6f0bc2a35e2
HajkD <hgd23@cam.ac.uk>
2016-10-28 15:19:04 +0100

    add new function to list number of available species in each kingdom of life

---
+++ R/listKingdoms.R
@@ -0,0 +1,37 @@
+#' @title List number of available genomes in each kingdom of life
+#' @description Users can retrieve the available number of sequenced genomes per kingdom.
+#' @param db a character string specifying the database for which genome availability shall be checked, 
+#' e.g. \code{db = "refseq"}, \code{db = "genbank"}, \code{db = "ensembl"}, \code{db = "ensemblgenomes"}. 
+#' @author Hajk-Georg Drost
+#' @examples 
+#' \dontrun {
+#' 
+#' }
+#' @seealso \code{\link{listGenomes}}, \code{\link{is.genome.available}}
+#' @export
+
+listKingdoms <- function(db = "refseq") {
+    
+    listgenomes.data <-
+        listGenomes(db = db, type = "kingdom", details = TRUE)
+    
+    if (is.element(db, c("refseq", "genbank"))) {
+        uniq.species <-
+            dplyr::summarise(
+                dplyr::group_by(listgenomes.data, organism_name, kingdoms),
+                unique_elements = dplyr::n_distinct(organism_name)
+            )
+        
+        return(table(uniq.species$kingdoms))
+    }
+    
+    if (is.element(db, c("ensembl", "ensemblgenomes"))) {
+        uniq.species <-
+            dplyr::summarise(
+                dplyr::group_by(listgenomes.data, name, division),
+                unique_elements = dplyr::n_distinct(name)
+            )
+        
+        return(table(uniq.species$division))
+    }
+}
