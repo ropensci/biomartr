83d856d0a0d8609fad69f664269297eab006dbaa
HajkD <hgd23@cam.ac.uk>
2016-10-28 12:09:59 +0100

    update unit test

--- tests/testthat/test-is.genome.available.R
+++ tests/testthat/test-is.genome.available.R
@@ -5,8 +5,8 @@ test_that("The is.genome.available() interface works properly..",{
         skip_on_cran()
         
         # test whether interface to 'refseq' works properly
-        g <- is.genome.available(organism = "Arabidopsis thaliana", details = TRUE, db = "refseq")
-        expect_identical(as.character(g[1, 1]),"Arabidopsis thaliana")
+        g <- is.genome.available(db = "refseq", organism = "Arabidopsis thaliana", details = TRUE)
+        expect_identical(g$organism_name,"Arabidopsis thaliana")
         
         # test with a second run using locally stored information
         is.genome.available(organism = "Homo sapiens", details = TRUE)
@@ -15,19 +15,25 @@ test_that("The is.genome.available() interface works properly..",{
         is.genome.available(organism = "Homo sapiens")
         
         # test whether interface to 'genbank' works properly
-        is.genome.available(organism = "Homo sapiens", details = TRUE, db = "genbank")
+        is.genome.available(db = "genbank", organism = "Homo sapiens", details = TRUE)
         
         # test whether it works properly a second time based on the internal files
-        is.genome.available(organism = "Arabidopsis thaliana", details = TRUE, db = "genbank")
+        is.genome.available(db = "genbank", organism = "Arabidopsis thaliana", details = TRUE)
         
         # test whether it works properly a second time based on the internal files without details
-        is.genome.available(organism = "Arabidopsis thaliana", db = "genbank")
+        is.genome.available(db = "genbank", organism = "Arabidopsis thaliana")
         
         # test whether interface to 'ensembl' works properly
-        is.genome.available(organism = "Homo sapiens", details = TRUE, db = "ensembl")
+        is.genome.available(db = "ensembl", organism = "Homo sapiens", details = TRUE)
         
         # test whether interface to 'ensembl' works properly without details
-        is.genome.available(organism = "Homo sapiens", db = "ensembl")
+        is.genome.available(db = "ensembl", organism = "Homo sapiens")
+        
+        # test whether interface to 'ensemblgenomes' works properly
+        is.genome.available(db = "ensemblgenomes", organism = "Arabidopsis thaliana", details = TRUE)
+        
+        # test whether interface to 'ensemblgenomes' works properly without details
+        is.genome.available(db = "ensemblgenomes", organism = "Arabidopsis thaliana")
         
 })
 
