ccdf41b89f168ad59228237773d8da54246965be
HajkD <hgd23@cam.ac.uk>
2016-11-11 16:27:02 +0000

    add unit test

---
+++ tests/testthat/test-download.database.R
@@ -0,0 +1,19 @@
+context("Test: download.database()")
+
+test_that("The download.database() interface works properly..",{
+    
+    skip_on_cran()
+    skip_on_travis()
+    
+    download.database(db = "nr.27.tar.gz")
+})
+
+test_that("The download.database() throws error when wrong input database is specified..",{
+    
+    skip_on_cran()
+    skip_on_travis()
+    
+    expect_error(download.database(db = "nr..tar.gz"), "The specified database 'nr..tar.gz' could not be found on NCBI. Please use the listDatabases() command to retrieve available databases or check if the name was written correctly.")
+    expect_error(download.database(db = "nl"), "No entries for db = 'nl' could not be found.")
+    
+})
