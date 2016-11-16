71cd1599fc2386d4416253aeff37e22373deaea4
HajkD <hgd23@cam.ac.uk>
2016-11-16 20:40:40 +0000

    update internal error handling.. now corrupt files will not interrupt the meta.retrieval.all() download process

--- R/getENSEMBL.Annotation.R
+++ R/getENSEMBL.Annotation.R
@@ -56,12 +56,15 @@ getENSEMBL.Annotation <- function(organism, type = "dna", id.type = "toplevel", 
     }
     
     if (!is.element(stringr::str_to_lower(new.organism),
-                    ensembl.available.organisms$name))
-        stop(
+                    ensembl.available.organisms$name)) {
+        
+        warning(
             "Unfortunately organism '",
             organism,
-            "' is not available at ENSEMBL. Please check whether or not the organism name is typed correctly.", call. = FALSE
+            "' is not available at ENSEMBL. Please check whether or not the organism name is typed correctly. Thus, download of this species has been omitted."
         )
+        return(FALSE)
+    }
     
     # test proper API access
     tryCatch({
