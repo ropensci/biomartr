bec02a043b5333fcbb3bca19c0ecf9a794c180dc
HajkD <hgd23@cam.ac.uk>
2016-11-16 20:41:29 +0000

    update internal error handling.. now corrupt files will not interrupt the meta.retrieval.all() download process

--- R/getGenome.R
+++ R/getGenome.R
@@ -183,65 +183,69 @@ getGenome <-
             # download genome sequence from ENSEMBL
             genome.path <- getENSEMBL.Seq(organism, type = "dna", id.type = "toplevel", path)
             
-            new.organism <- stringr::str_replace_all(organism," ","_")
-            
-            # test proper API access
-            tryCatch({
-                json.qry.info <-
-                    jsonlite::fromJSON(
-                        paste0(
-                            "http://rest.ensembl.org/info/assembly/",
-                            new.organism,
-                            "?content-type=application/json"
+            if (is.logical(genome.path)) {
+                invisible(return(TRUE))
+            } else {
+                new.organism <- stringr::str_replace_all(organism," ","_")
+                
+                # test proper API access
+                tryCatch({
+                    json.qry.info <-
+                        jsonlite::fromJSON(
+                            paste0(
+                                "http://rest.ensembl.org/info/assembly/",
+                                new.organism,
+                                "?content-type=application/json"
+                            )
                         )
+                }, error = function(e)
+                    stop(
+                        "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?", call. = FALSE
+                    ))
+                
+                cwd <- getwd()
+                
+                setwd(path)
+                
+                # generate Genome documentation
+                sink(paste0("doc_",new.organism,"_db_",db,".txt"))
+                
+                cat(paste0("File Name: ", genome.path))
+                cat("\n")
+                cat(paste0("Organism Name: ", new.organism))
+                cat("\n")
+                cat(paste0("Database: ", db))
+                cat("\n")
+                cat(paste0("Download_Date: ", date()))
+                cat("\n")
+                cat(paste0("assembly_name: ", json.qry.info$assembly_name))
+                cat("\n")
+                cat(paste0("assembly_date: ", json.qry.info$assembly_date))
+                cat("\n")
+                cat(paste0("genebuild_last_geneset_update: ", json.qry.info$genebuild_last_geneset_update))
+                cat("\n")
+                cat(paste0("assembly_accession: ", json.qry.info$assembly_accession))
+                cat("\n")
+                cat(paste0("genebuild_initial_release_date: ", json.qry.info$genebuild_initial_release_date))
+                
+                sink()
+                
+                setwd(cwd)
+                
+                print(
+                    paste0(
+                        "The genome of '",
+                        organism,
+                        "' has been downloaded to '",
+                        path,
+                        "' and has been named '",
+                        basename(genome.path),
+                        "'."
                     )
-            }, error = function(e)
-                stop(
-                    "The API 'http://rest.ensembl.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensembl.org' currently available?", call. = FALSE
-                ))
-            
-            cwd <- getwd()
-            
-            setwd(path)
-            
-            # generate Genome documentation
-            sink(paste0("doc_",new.organism,"_db_",db,".txt"))
-            
-            cat(paste0("File Name: ", genome.path))
-            cat("\n")
-            cat(paste0("Organism Name: ", new.organism))
-            cat("\n")
-            cat(paste0("Database: ", db))
-            cat("\n")
-            cat(paste0("Download_Date: ", date()))
-            cat("\n")
-            cat(paste0("assembly_name: ", json.qry.info$assembly_name))
-            cat("\n")
-            cat(paste0("assembly_date: ", json.qry.info$assembly_date))
-            cat("\n")
-            cat(paste0("genebuild_last_geneset_update: ", json.qry.info$genebuild_last_geneset_update))
-            cat("\n")
-            cat(paste0("assembly_accession: ", json.qry.info$assembly_accession))
-            cat("\n")
-            cat(paste0("genebuild_initial_release_date: ", json.qry.info$genebuild_initial_release_date))
-            
-            sink()
-            
-            setwd(cwd)
-            
-            print(
-                paste0(
-                    "The genome of '",
-                    organism,
-                    "' has been downloaded to '",
-                    path,
-                    "' and has been named '",
-                    basename(genome.path),
-                    "'."
                 )
-            )
-            
-            return(genome.path)
+                
+                return(genome.path)   
+            }
         }
         
         if (db == "ensemblgenomes") {
@@ -254,65 +258,69 @@ getGenome <-
             # download genome sequence from ENSEMBLGENOMES
             genome.path <- getENSEMBLGENOMES.Seq(organism, type = "dna", id.type = "toplevel", path)
             
-            new.organism <- stringr::str_replace_all(organism," ","_")
-            
-            # test proper API access
-            tryCatch({
-                json.qry.info <-
-                    jsonlite::fromJSON(
-                        paste0(
-                            "http://rest.ensemblgenomes.org/info/assembly/",
-                            new.organism,
-                            "?content-type=application/json"
+            if (is.logical(genome.path)) {
+                invisible(return(TRUE))
+            } else {
+                new.organism <- stringr::str_replace_all(organism," ","_")
+                
+                # test proper API access
+                tryCatch({
+                    json.qry.info <-
+                        jsonlite::fromJSON(
+                            paste0(
+                                "http://rest.ensemblgenomes.org/info/assembly/",
+                                new.organism,
+                                "?content-type=application/json"
+                            )
                         )
+                }, error = function(e)
+                    stop(
+                        "The API 'http://rest.ensemblgenomes.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensemblgenomes.org' currently available?", call. = FALSE
+                    ))
+                
+                cwd <- getwd()
+                
+                setwd(path)
+                
+                # generate Genome documentation
+                sink(paste0("doc_",new.organism,"_db_",db,".txt"))
+                
+                cat(paste0("File Name: ", genome.path))
+                cat("\n")
+                cat(paste0("Organism Name: ", new.organism))
+                cat("\n")
+                cat(paste0("Database: ", db))
+                cat("\n")
+                cat(paste0("Download_Date: ", date()))
+                cat("\n")
+                cat(paste0("assembly_name: ", json.qry.info$assembly_name))
+                cat("\n")
+                cat(paste0("assembly_date: ", json.qry.info$assembly_date))
+                cat("\n")
+                cat(paste0("genebuild_last_geneset_update: ", json.qry.info$genebuild_last_geneset_update))
+                cat("\n")
+                cat(paste0("assembly_accession: ", json.qry.info$assembly_accession))
+                cat("\n")
+                cat(paste0("genebuild_initial_release_date: ", json.qry.info$genebuild_initial_release_date))
+                
+                sink()
+                
+                setwd(cwd)
+                
+                print(
+                    paste0(
+                        "The genome of '",
+                        organism,
+                        "' has been downloaded to '",
+                        path,
+                        "' and has been named '",
+                        basename(genome.path),
+                        "'."
                     )
-            }, error = function(e)
-                stop(
-                    "The API 'http://rest.ensemblgenomes.org' does not seem to work properly. Are you connected to the internet? Is the homepage 'http://rest.ensemblgenomes.org' currently available?", call. = FALSE
-                ))
-            
-            cwd <- getwd()
-            
-            setwd(path)
-            
-            # generate Genome documentation
-            sink(paste0("doc_",new.organism,"_db_",db,".txt"))
-            
-            cat(paste0("File Name: ", genome.path))
-            cat("\n")
-            cat(paste0("Organism Name: ", new.organism))
-            cat("\n")
-            cat(paste0("Database: ", db))
-            cat("\n")
-            cat(paste0("Download_Date: ", date()))
-            cat("\n")
-            cat(paste0("assembly_name: ", json.qry.info$assembly_name))
-            cat("\n")
-            cat(paste0("assembly_date: ", json.qry.info$assembly_date))
-            cat("\n")
-            cat(paste0("genebuild_last_geneset_update: ", json.qry.info$genebuild_last_geneset_update))
-            cat("\n")
-            cat(paste0("assembly_accession: ", json.qry.info$assembly_accession))
-            cat("\n")
-            cat(paste0("genebuild_initial_release_date: ", json.qry.info$genebuild_initial_release_date))
-            
-            sink()
-            
-            setwd(cwd)
-            
-            print(
-                paste0(
-                    "The genome of '",
-                    organism,
-                    "' has been downloaded to '",
-                    path,
-                    "' and has been named '",
-                    basename(genome.path),
-                    "'."
                 )
-            )
-            
-            return(genome.path)
+                
+                return(genome.path)
+            }   
         }
     }
 
