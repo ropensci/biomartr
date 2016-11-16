f5206b86e35537908e283d7736614c149f7fa4a0
HajkD <hgd23@cam.ac.uk>
2016-11-16 14:54:36 +0000

    update docu

--- R/getSubgroups.R
+++ R/getSubgroups.R
@@ -1,22 +1,41 @@
+#' @title Retrieve available subgroups for a kingdom of life
+#' @description A short list of available subgroups for a kingdom of life.
+#' @param kingdom a character string specifying for which kingdom of life subgroups shall be retrieved. See \code{\link{getKingdoms}} for details.
+#' @param db a character string specifying the database from which the genome shall be retrieved: \code{db = "refseq"}, \code{db = "genbank"}.
+#' Default is \code{db = "refseq"}.
+#' @author Hajk-Georg Drost
+#' @examples 
+#' # get possible kigdom names
+#' getKingdoms(db = "refseq")
+#' # retrieve subgroups for vertebrate_mammalian available at refseq
+#' getSubgroups(kingdom = "vertebrate_mammalian", db = "refseq")
+#' 
+#' # get possible kigdom names
+#' getKingdoms(db = "genbank")
+#' # retrieve subgroups for vertebrate_mammalian available at genbank
+#' getSubgroups(kingdom = "vertebrate_mammalian",db = "genbank")
+#' @seealso \code{\link{meta.retrieval}}, \code{\link{getGenome}}, \code{\link{getProteome}}, \code{\link{getCDS}}
+#' @export
+
 getSubgroups <- function(kingdom, db = "refseq") {
     
     if (!is.element(kingdom, getKingdoms(db = db)))
         stop(paste0(
             "Please select a valid kingdom: ",
-            paste0(getKingdoms(), collapse = ", ")
+            paste0(getKingdoms(db = db), collapse = ", ")
         ), call. = FALSE)
     
-    if (!is.element(db, c("refseq", "genbank","ensemblgenomes")))
-        stop("Please select one of the available data bases: 'refseq', 'genbank', 'ensemblgenomes'", call. = FALSE)
+    if (!is.element(db, c("refseq", "genbank")))
+        stop("Please select one of the available data bases: 'refseq', 'genbank'.", call. = FALSE)
     
     # get Kingdom Assembly Summary file
     AssemblyFilesAllKingdoms <- getKingdomAssemblySummary(db = db)
     
     # get all genomes list
-    all.genomes <- listGenomes("all", TRUE, db = "all")
+    all.genomes <- listGenomes(db = db, type = "subgroup", details = TRUE)
     
     # join tables
     joined.df <- dplyr::inner_join(AssemblyFilesAllKingdoms,all.genomes, by = "organism_name")
     
-    return(names(table(joined.df$subgroups)))
+    return(names(table(joined.df$subgroup)))
 }
