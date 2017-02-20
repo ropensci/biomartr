14cd29047ebbe3372a9385276a9b781824b6d8a2
HajkD <hgd23@cam.ac.uk>
2017-02-20 13:41:30 +0000

    add new func to perform ENSEMBL Info retrieval

---
+++ R/getENSEMBLInfo.R
@@ -0,0 +1,9 @@
+#' @title Retrieve ENSEMBL info file
+#' @description Retrieve species and genome information from http://rest.ensembl.org/info/species?content-type=application/json/.
+#' @author Hajk-Georg Drost
+#' @export 
+getENSEMBLInfo <- function() {
+    
+    ENSEMBLInfoTable <- get.ensembl.info(update = TRUE)
+    return(ENSEMBLInfoTable)
+}
