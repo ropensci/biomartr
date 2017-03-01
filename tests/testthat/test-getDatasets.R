256adbc472bd495e1d09d617c0ae78af29d03103
HajkD <hgd23@cam.ac.uk>
2017-03-01 19:46:46 +0000

    update unit test

--- tests/testthat/test-getDatasets.R
+++ tests/testthat/test-getDatasets.R
@@ -13,7 +13,7 @@ test_that("The getDatasets() interface works properly..",{
         marts <- getMarts()[ , "mart"]
         ex_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
         
-        attrib <- getDatasets(mart = as.character(marts[ex_mart]))
+        attrib <- getDatasets(mart = as.character(marts$mart[ex_mart]))
         
         expect_true(equal_lists(as.list(attrib[1 , ]), list(dataset = "oanatinus_gene_ensembl",
                                                             description = "Platypus genes (OANA5)",
