5f737a56222c6901a2f27bb19b65a6986995e45b
HajkD <hgd23@cam.ac.uk>
2017-03-01 19:46:43 +0000

    update unit test

--- tests/testthat/test-getAttributes.R
+++ tests/testthat/test-getAttributes.R
@@ -13,7 +13,7 @@ test_that("The getAttributes() interface works properly..",{
         marts <- getMarts()[ , "mart"]
         ensemb_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
         
-        attrib <- getAttributes(mart = as.character(marts[ensemb_mart]), dataset = "hsapiens_gene_ensembl")
+        attrib <- getAttributes(mart = as.character(marts$mart[ensemb_mart]), dataset = "hsapiens_gene_ensembl")
         
         expect_true(equal_lists(as.list(attrib[1 , ]), list(name = "ensembl_gene_id",
                                                             description = "Gene ID")))
