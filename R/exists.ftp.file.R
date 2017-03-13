4e830009d5bc40b0d8532ec804997ade40741265
HajkD <hgd23@cam.ac.uk>
2017-03-13 14:53:20 +0000

    add system sleep to not viotate the NCBI query convention

--- R/exists.ftp.file.R
+++ R/exists.ftp.file.R
@@ -2,6 +2,7 @@ exists.ftp.file <- function(url, file.path) {
     con <- curl::curl(url = url, "rb", curl::new_handle(dirlistonly = TRUE))
     ftp.content <- suppressMessages(readr::read_delim(con, delim = "\n", col_names = FALSE))
     close(con)
+    Sys.sleep(0.33)
     if (is.element(as.character(basename(file.path)), as.character(ftp.content$X1)))
         return(TRUE)
     else 
