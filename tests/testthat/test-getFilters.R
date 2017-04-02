772c6c28d1aada402719b69277cb4d097359b9ab
HajkD <hgd23@cam.ac.uk>
2017-04-02 18:57:27 +0100

    update unit test

--- tests/testthat/test-getFilters.R
+++ tests/testthat/test-getFilters.R
@@ -9,13 +9,12 @@ test_that("The getFilters() interface works properly..",{
         
         skip_on_cran()
         
-        marts <- getMarts()[ , "mart"]
+        marts <- getMarts()$mart
         elsembl_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
         
-        attrib <- getFilters(mart = as.character(marts$mart[elsembl_mart]), dataset = "hsapiens_gene_ensembl")
+        attrib <- getFilters(mart = as.character(marts[elsembl_mart]), dataset = "hsapiens_gene_ensembl")
         
-        expect_true(equal_lists(as.list(attrib[1 , ]), list(name = "chromosome_name",
-                                                            description = "Chromosome name")))
+        expect_true(equal_lists(as.list(attrib[1 , ]), list(name = "chromosome_name")))
 })
 
 
