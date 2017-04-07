e69ce5454f84e3a5dd329ffa26dcfc3a9e7cdd24
HajkD <hgd23@cam.ac.uk>
2017-04-07 15:01:48 +0100

    format to 80 line

--- R/getMarts.R
+++ R/getMarts.R
@@ -1,16 +1,13 @@
-#' @title Retrieve All Available BioMart Databases
-#' @description This funcion queries the BioMart API and returns a table
-#' storing all available BioMart databases.
-#' 
+#' @title Retrieve information about available Ensembl Biomart databases 
+#' @description This funcion queries the Ensembl Biomart API and returns a table
+#' storing information about all available Ensembl Biomart databases.
 #' @author Hajk-Georg Drost
 #' @examples
-#' 
-#' \dontrun{
-#' # get a table of all available databases from BioMart
+#' # get a table of all available databases from Ensembl Biomart
 #'  getMarts()
-#' }
-#' 
-#' @seealso \code{\link{getDatasets}}, \code{\link{getAttributes}}, \code{\link{getFilters}}, \code{\link{organismBM}}, \code{\link{organismFilters}}, \code{\link{organismAttributes}}
+#' @seealso \code{\link{getDatasets}}, \code{\link{getAttributes}}, 
+#' \code{\link{getFilters}}, \code{\link{organismBM}}, 
+#' \code{\link{organismFilters}}, \code{\link{organismAttributes}}
 #' @export
 getMarts <- function(){
     
