c6461b255bc4a48819058f3e8def8e4ba9aab3dd
HajkD <hgd23@cam.ac.uk>
2017-04-08 14:39:22 +0100

    1:ncol() -> seq_len()

--- R/getFilters.R
+++ R/getFilters.R
@@ -102,7 +102,7 @@ getFilters <- function(mart, dataset){
                              )), 1, function(x)
                                  unlist(strsplit(x, "\t")))))
         
-        colnames(rawDF) <- paste0("V", 1:ncol(rawDF))
+        colnames(rawDF) <- paste0("V", seq_len(ncol(rawDF)))
         
         filterBioMart <-
             as.data.frame(rawDF[, c("V1", "V2")],
