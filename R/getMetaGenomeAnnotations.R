9d4dda67af4d129e0803124e5ed3372c63f6b502
HajkD <hgd23@cam.ac.uk>
2017-04-08 14:39:29 +0100

    change sapply -> lapply

--- R/getMetaGenomeAnnotations.R
+++ R/getMetaGenomeAnnotations.R
@@ -108,6 +108,6 @@ getMetaGenomeAnnotations <-
         
         file.paths <- file.path(path, list.files(path = path))
         # return only file paths without "*.txt"
-        return(file.paths[!unlist(sapply(file.paths, function(x)
+        return(file.paths[!unlist(lapply(file.paths, function(x)
             stringr::str_detect(x, "[.]txt")))])
 }
