3356ae4701720eeed7496da4a625eebc328e9cca
HajkD <hgd23@cam.ac.uk>
2017-04-09 10:32:04 +0100

    format to 80 line

--- tests/testthat/test-getFilters.R
+++ tests/testthat/test-getFilters.R
@@ -9,12 +9,15 @@ test_that("The getFilters() interface works properly..",{
         
         skip_on_cran()
         
-        marts <- getMarts()$mart
-        elsembl_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
-        
-        attrib <- getFilters(mart = as.character(marts[elsembl_mart]), dataset = "hsapiens_gene_ensembl")
-        
-        expect_equal(attrib[1 , 1], "chromosome_name")
+    marts <- getMarts()$mart
+    elsembl_mart <-
+        which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
+    
+    attrib <-
+        getFilters(mart = as.character(marts[elsembl_mart]), 
+                   dataset = "hsapiens_gene_ensembl")
+    
+    expect_equal(attrib[1 , 1], "chromosome_name")
         
 })
 
