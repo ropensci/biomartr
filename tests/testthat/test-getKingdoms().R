86de96edc4bcebea54cf920e4fc1e72575a31b56
HajkD <hgd23@cam.ac.uk>
2017-04-09 10:32:21 +0100

    format to 80 line

--- tests/testthat/test-getKingdoms().R
+++ tests/testthat/test-getKingdoms().R
@@ -3,8 +3,21 @@ context("Test: getKingdoms()")
 test_that("The getKingdoms() interface works properly..",{
     
     skip_on_cran()
-    skip_on_travis() 
-    testthat::equals(getKingdoms(), c("archaea", "bacteria", "fungi", "invertebrate", "plant", "protozoa", "vertebrate_mammalian", "vertebrate_other", "viral"))
-
+    skip_on_travis()
+    testthat::equals(
+        getKingdoms(),
+        c(
+            "archaea",
+            "bacteria",
+            "fungi",
+            "invertebrate",
+            "plant",
+            "protozoa",
+            "vertebrate_mammalian",
+            "vertebrate_other",
+            "viral"
+        )
+    )
+    
 })
 
