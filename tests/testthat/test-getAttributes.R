d0496de483a329250a1f7568f98b7972884b095f
HajkD <hgd23@cam.ac.uk>
2017-04-09 10:31:57 +0100

    format to 80 line

--- tests/testthat/test-getAttributes.R
+++ tests/testthat/test-getAttributes.R
@@ -10,10 +10,13 @@ test_that("The getAttributes() interface works properly..",{
         
         skip_on_cran()
         
-        marts <- getMarts()$mart
-        ensemb_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
-        
-        attrib <- getAttributes(mart = as.character(marts[ensemb_mart]), dataset = "hsapiens_gene_ensembl")
-        
-            expect_equal(attrib[1 , 1], "ensembl_gene_id")
+    marts <- getMarts()$mart
+    ensemb_mart <-
+        which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
+    
+    attrib <-
+        getAttributes(mart = as.character(marts[ensemb_mart]), 
+                      dataset = "hsapiens_gene_ensembl")
+    
+    expect_equal(attrib[1 , 1], "ensembl_gene_id")
 })
