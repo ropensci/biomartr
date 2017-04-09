c6ad6c56e18725a1265cf4c0f094c4d41626bda0
HajkD <hgd23@cam.ac.uk>
2017-04-09 10:32:01 +0100

    format to 80 line

--- tests/testthat/test-getDatasets.R
+++ tests/testthat/test-getDatasets.R
@@ -10,14 +10,21 @@ test_that("The getDatasets() interface works properly..",{
         
         skip_on_cran()
         
-        marts <- getMarts()[ , "mart"]
-        ex_mart <- which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
-        
-        attrib <- getDatasets(mart = as.character(marts$mart[ex_mart]))
-        
-        expect_true(equal_lists(as.list(attrib[1 , ]), list(dataset = "oanatinus_gene_ensembl",
-                                                            description = "Platypus genes (OANA5)",
-                                                            version = "OANA5")))
+    marts <- getMarts()[, "mart"]
+    ex_mart <-
+        which(stringr::str_match(marts, "ENSEMBL_MART_ENSEMBL") != "NA")
+    
+    attrib <-
+        getDatasets(mart = as.character(marts$mart[ex_mart]))
+    
+    expect_true(equal_lists(
+        as.list(attrib[1 , ]),
+        list(
+            dataset = "oanatinus_gene_ensembl",
+            description = "Platypus genes (OANA5)",
+            version = "OANA5"
+        )
+    ))
 })
 
 
