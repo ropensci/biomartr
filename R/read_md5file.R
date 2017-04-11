422cafe7ac085b8e2f3df53d5878f8cc738346af
HajkD <hgd23@cam.ac.uk>
2017-04-11 19:25:18 +0100

    helper function to read md5 files

---
+++ R/read_md5file.R
@@ -0,0 +1,22 @@
+#' @title Helper function to read md5checksum file from NCBI
+#' @description NCBI stores md5checksum.txt files that can be retrieved
+#' to check whether or not the downloaded file is corrupt.
+#' @param md5path path to md5checksum.txt file.
+#' @author Hajk-Georg Drost
+#' @noRd
+read_md5file <- function(md5path) {
+    
+    file <-
+        readr::read_delim(
+            md5path,
+            delim = "  ",
+            col_names = c("md5", "na", "file_name"),
+            col_types = readr::cols(
+                md5 = readr::col_character(),
+                na = readr::col_character(),
+                file_name = readr::col_character()
+            )
+        )
+    
+    return(file)
+}
