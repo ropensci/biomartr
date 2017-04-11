d956280480ccb404db2b8fd254a36d4aea5d3d79
HajkD <hgd23@cam.ac.uk>
2017-04-11 09:58:56 +0100

    update docu

--- R/getENSEMBLInfo.R
+++ R/getENSEMBLInfo.R
@@ -2,6 +2,9 @@
 #' @description Retrieve species and genome information from 
 #' http://rest.ensembl.org/info/species?content-type=application/json/.
 #' @author Hajk-Georg Drost
+#' @examples 
+#' info.file <- getENSEMBLInfo()
+#' info.file
 #' @export 
 getENSEMBLInfo <- function() {
     ENSEMBLInfoTable <- get.ensembl.info(update = TRUE)
