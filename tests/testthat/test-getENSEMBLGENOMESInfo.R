9008f0e512a1d080d4304c9566388f04c0840d09
HajkD <hgd23@cam.ac.uk>
2017-04-11 09:59:34 +0100

    add unit test

---
+++ tests/testthat/test-getENSEMBLGENOMESInfo.R
@@ -0,0 +1,7 @@
+context("Test: getENSEMBLGENOMESInfo()")
+
+test_that("The getENSEMBLGENOMESInfo() interface works properly..",{
+    
+    # ENSEMBLGENOMES Info file retrieval
+    getENSEMBLGENOMESInfo()
+})
