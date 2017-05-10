46461ebfc0038c4edfc828749649805ade5be95a
HajkD <hajkdrost@googlemail.com>
2017-05-10 17:13:17 +0100

    wrap examples

--- R/getENSEMBLGENOMESInfo.R
+++ R/getENSEMBLGENOMESInfo.R
@@ -2,9 +2,11 @@
 #' @description Retrieve species and genome information from 
 #' http://rest.ensemblgenomes.org/info/species?content-type=application/json/.
 #' @author Hajk-Georg Drost
-#' @examples 
+#' @examples
+#' \dontrun{ 
 #' info.file <- getENSEMBLGENOMESInfo()
 #' info.file
+#' }
 #' @export 
 getENSEMBLGENOMESInfo <- function() {
     ENSEMBLGENOMESInfoTable <- get.ensemblgenome.info(update = TRUE)
