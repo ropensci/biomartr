237f1789d19a6b0322000ef8e0e57ba2e33e030e
HajkD <hgd23@cam.ac.uk>
2017-05-18 11:40:43 +0100

    fix bug in which genomes having no representative genome would generate NULL paths that subsequently crashed the meta.retrieval function when it tried to print out the result paths

--- R/getRNA.R
+++ R/getRNA.R
@@ -81,6 +81,7 @@ getRNA <-
                         "'. Thus, download for this species has been omitted."
                     )
                 )
+                return("Not available")
             } else {
                 if (nrow(FoundOrganism) > 1) {
                     warnings(
