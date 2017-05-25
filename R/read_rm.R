7806b82c71e3f83048204be46e4b3463669ed0f0
HajkD <hgd23@cam.ac.uk>
2017-05-25 17:03:14 +0100

    new function to import repeat masker output

---
+++ R/read_rm.R
@@ -0,0 +1,59 @@
+#' @title Import Repeat Masker output file
+#' @description This function reads an organism specific 
+#' Repeat Masker output file.
+#' @param file a character string specifying the path to the file 
+#' storing the Repeat Masker output (e.g. retrieved with \code{\link{getRepeatMasker}}).
+#' @author Hajk-Georg Drost
+#' @details This function takes a string specifying the path to the 
+#' Repeat Masker output file of interest as first argument.
+#' @seealso \code{\link{getRepeatMasker}}, \code{\link{read_genome}}, 
+#' \code{\link{read_proteome}}, \code{\link{read_gff}}, \code{\link{read_rna}}
+#' @export
+
+read_rm <- function(file) {
+    
+    rm_file <- readr::read_lines(file = file, skip = 3)
+    rm_file <- lapply(rm_file, function(x) {
+        str.res <- unlist(stringr::str_split(x, "\\s+"))[-1]
+        str.res <- str.res[1:14]
+        return(str.res)
+    })
+    
+    
+    rm_file <- tibble::as_tibble(do.call(rbind, rm_file))
+    colnames(rm_file) <- c(
+        "sw_score",
+        "perc_div",
+        "perc_del",
+        "perc_insert",
+        "qry_id",
+        "qry_start",
+        "qry_end",
+        "qry_left",
+        "matching_repeat",
+        "repeat_id",
+        "matching_class",
+        "no_bp_in_complement",
+        "in_repeat_start",
+        "in_repeat_end"
+    )
+    
+    rm_file <- dplyr::mutate(rm_file,
+                             qry_start = as.numeric(qry_start),
+                             qry_end = as.numeric(qry_end))
+    
+    rm_file <-
+        dplyr::filter(
+            rm_file,
+            !is.na(qry_start),
+            !is.na(qry_end)
+        )
+    
+    rm_file <-
+        dplyr::mutate(
+            rm_file,
+            qry_width = qry_end - qry_start + 1)
+    return(rm_file)
+}
+
+
