085348a9480885f4c1cc4c2700a3956c7e8b90b6
HajkD <hajkdrost@googlemail.com>
2017-09-20 12:00:16 +0100

    unit test takes to long and thus has been removed

--- tests/testthat/test-organismAttributes.R
+++ tests/testthat/test-organismAttributes.R
@@ -8,7 +8,5 @@ equal_lists <- function(df1, df2) {
 test_that("The organismAttributes() interface works properly..",{
         
         skip_on_cran()
-        
-        oattr <- organismAttributes("Homo sapiens")
-        expect_identical(as.character(oattr[1, 1]),"ensembl_gene_id")
+
 })
