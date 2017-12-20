2342da19eea9c66ee960b70d23de6b0445476e21
HajkD <hajkdrost@googlemail.com>
2017-12-20 12:48:28 +0000

    update unit test

--- tests/testthat/test-organismFilters.R
+++ tests/testthat/test-organismFilters.R
@@ -8,6 +8,7 @@ equal_lists <- function(df1, df2) {
 test_that("The organismFilters() interface works properly..",{
         
         skip_on_cran()
+        skip_on_travis()
         
         oF <- organismFilters("Homo sapiens")
         expect_identical(as.character(oF[1, 1]),"chromosome_name")
