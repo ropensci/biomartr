40c9838f58be362e4e31f65f2aef43dbdeb4156d
HajkD <hajkdrost@googlemail.com>
2017-12-20 12:48:11 +0000

    update unit test

--- tests/testthat/test-getAttributes.R
+++ tests/testthat/test-getAttributes.R
@@ -9,6 +9,7 @@ equal_lists <- function(df1, df2) {
 test_that("The getAttributes() interface works properly..",{
         
         skip_on_cran()
+        skip_on_travis()
         
     marts <- getMarts()$mart
     ensemb_mart <-
@@ -24,7 +25,8 @@ test_that("The getAttributes() interface works properly..",{
 test_that("The getAttributes() throws error when wrong mart is selected",{
     
     skip_on_cran()
-    
+    skip_on_travis()
+        
     expect_error(getAttributes(mart = "somethingelse",
                   dataset = "hsapiens_gene_ensembl"))
     
@@ -33,7 +35,8 @@ test_that("The getAttributes() throws error when wrong mart is selected",{
 test_that("The getAttributes() throws error when wrong dataset is selected",{
     
     skip_on_cran()
-    
+    skip_on_travis()
+        
     expect_error(getAttributes(mart = "hsapiens_gene_ensembl",
                   dataset = "somethingelse"))
     
