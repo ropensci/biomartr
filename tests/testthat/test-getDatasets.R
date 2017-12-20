4645992f5b4c99e22bdf4bb0cf90e9406671566d
HajkD <hajkdrost@googlemail.com>
2017-12-20 12:48:14 +0000

    update unit test

--- tests/testthat/test-getDatasets.R
+++ tests/testthat/test-getDatasets.R
@@ -9,6 +9,7 @@ equal_lists <- function(df1, df2) {
 test_that("The getDatasets() interface works properly..",{
         
         skip_on_cran()
+        skip_on_travis()
         
     marts <- getMarts()[, "mart"]
     ex_mart <-
