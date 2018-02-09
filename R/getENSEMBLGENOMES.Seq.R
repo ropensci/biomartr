dbf784f9fc0d0d3b72d25028ad1d64d3a31d3576
HajkD <hgd23@cam.ac.uk>
2018-02-09 14:21:04 +0000

    add new optionality to use taxid and accession specification to retrieve genomes

--- R/getENSEMBLGENOMES.Seq.R
+++ R/getENSEMBLGENOMES.Seq.R
@@ -24,82 +24,13 @@ getENSEMBLGENOMES.Seq <-
         # test if REST API is responding
         is.ensemblgenomes.alive()
         
-        if (file.exists(file.path(tempdir(), "ensemblgenomes_summary.txt"))) {
-            suppressWarnings(
-                ensembl.available.organisms <-
-                    readr::read_delim(
-                        file.path(tempdir(), "ensemblgenomes_summary.txt"),
-                        delim = "\t",
-                        quote = "\"",
-                        escape_backslash = FALSE,
-                        col_names = c(
-                            "division",
-                            "taxon_id",
-                            "name",
-                            "release",
-                            "display_name",
-                            "accession",
-                            "common_name",
-                            "assembly"
-                        ),
-                        col_types = readr::cols(
-                            division = readr::col_character(),
-                            taxon_id = readr::col_integer(),
-                            name = readr::col_character(),
-                            release = readr::col_integer(),
-                            display_name = readr::col_character(),
-                            accession = readr::col_character(),
-                            common_name = readr::col_character(),
-                            assembly = readr::col_character()
-                        ),
-                        comment = "#"
-                    )
-            )
-        }
-        
-        if (!file.exists(file.path(tempdir(), "ensemblgenomes_summary.txt"))) {
-            # check if organism is available on ENSEMBL
-            tryCatch({
-                ensembl.available.organisms <-
-                    jsonlite::fromJSON(
-     "http://rest.ensemblgenomes.org/info/species?content-type=application/json"
-                    )
-            }, error = function(e) {
-                warning(
-                    "The API 'http://rest.ensemblgenomes.org' does not seem to ",
-                    "work properly. Are you connected to the internet? ",
-                    "Is the homepage 'http://rest.ensemblgenomes.org' currently available?",
-                    call. = FALSE
-                )
-                return(FALSE)
-            })
-            
-            aliases <- groups <- NULL
-            
-            # transform list object returned by 'fromJSON' to tibble
-            ensembl.available.organisms <-
-                tibble::as_tibble(dplyr::select(
-                    ensembl.available.organisms$species,
-                    -aliases,
-                    -groups
-                ))
-            
-            readr::write_tsv(
-                ensembl.available.organisms,
-                file.path(tempdir(), "ensemblgenomes_summary.txt")
-            )
-        }
-        
-        if (!is.element(stringr::str_to_lower(new.organism),
-                        ensembl.available.organisms$name)) {
-            warning(
-                "Unfortunately organism '",
-                organism,
-                "' is not available at ENSEMBLGENOMES. Please check whether or ",
-                "not the organism name is typed correctly or try db = 'ensembl'. ",
-                "Thus, download of this species has been omitted.", call. = FALSE
-            )
-            return(FALSE)
+        if ( !suppressMessages(is.genome.available(organism = organism, db = "ensemblgenomes", details = FALSE)) ) {
+            stop("Unfortunately organism '", organism, "' is not available at ENSEMBLGENOMES. ",
+                    "Please check whether or not the organism name is typed correctly or try db = 'ensembl'.",
+                    " Thus, download of this species has been omitted. ", call. = FALSE)
+        } else {
+            ensembl_summary <- suppressMessages(is.genome.available(organism = organism, db = "ensemblgenomes", details = TRUE))
+            new.organism <- paste0(stringr::str_to_upper(stringr::str_sub(ensembl_summary$name, 1, 1)), stringr::str_sub(ensembl_summary$name, 2, nchar(ensembl_summary$name)))
         }
         
         # test proper API access
