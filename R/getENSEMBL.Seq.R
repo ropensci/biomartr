5aa141496e720c12187b2e718d5a8f7034eb56d6
HajkD <hgd23@cam.ac.uk>
2018-05-24 18:21:24 +0100

    new status check and fixing taxid retrieval

--- R/getENSEMBL.Seq.R
+++ R/getENSEMBL.Seq.R
@@ -15,137 +15,146 @@ getENSEMBL.Seq <- function(organism, type = "dna", id.type = "toplevel", path) {
         stop("Please a 'type' argument supported by this function: 
              'dna', 'cds', 'pep', 'ncrna'.")
     
-    new.organism <- stringr::str_replace_all(organism, " ", "_")
     
-    if ( !suppressMessages(is.genome.available(organism = organism, db = "ensembl", details = FALSE)) ) {
-        stop("Unfortunately organism '", organism, "' is not available at ENSEMBL. ",
-                "Please check whether or not the organism name is typed correctly or try db = 'ensemblgenomes'.",
-                " Thus, download of this species has been omitted. ", call. = FALSE)
-    } else {
+    ensembl_summary <-
+        suppressMessages(is.genome.available(
+            organism = organism,
+            db = "ensembl",
+            details = TRUE
+        ))
+    
+    if (nrow(ensembl_summary) > 1) {
+        if (is.taxid(organism)) {
             ensembl_summary <-
-                    suppressMessages(is.genome.available(
-                            organism = organism,
-                            db = "ensembl",
-                            details = TRUE
-                    ))
-            new.organism <-
-                    paste0(
-                            stringr::str_to_upper(stringr::str_sub(new.organism, 1, 1)),
-                            stringr::str_sub(new.organism, 2, nchar(new.organism))
-                    )
+                dplyr::filter(ensembl_summary, taxon_id == organism | !is.na(assembly))
+        } else {
+            
+            ensembl_summary <-
+                dplyr::filter(
+                    ensembl_summary,
+                    (name == stringr::str_to_lower(stringr::str_replace_all(organism, " ", "_"))) |
+                        (accession == organism) |
+                        !is.na(assembly)
+                )
+        }
     }
     
-    # test proper API access
-    tryCatch({
-        json.qry.info <-
-            jsonlite::fromJSON(
+    new.organism <- stringr::str_replace_all(ensembl_summary$display_name, " ", "_")
+    organism <- ensembl_summary$display_name
+    new.organism <-
+        paste0(
+            stringr::str_to_upper(stringr::str_sub(new.organism, 1, 1)),
+            stringr::str_sub(new.organism, 2, nchar(new.organism))
+        )
+    
+    
+    rest_url <- paste0(
+        "http://rest.ensembl.org/info/assembly/",
+        new.organism,
+        "?content-type=application/json"
+    )
+    
+    rest_api_status <- test_url_status(url = rest_url, organism = organism)   
+    if (is.logical(rest_api_status)) {
+        return(FALSE)
+    } else {
+        
+        # construct retrieval query
+        ensembl.qry <-
+            paste0(
+                "ftp://ftp.ensembl.org/pub/current_fasta/",
+                stringr::str_to_lower(new.organism),
+                "/",
+                type,
+                "/",
                 paste0(
-                    "http://rest.ensembl.org/info/assembly/",
                     new.organism,
-                    "?content-type=application/json"
+                    ".",
+                    rest_api_status$default_coord_system_version,
+                    ".",
+                    type,
+                    ifelse(id.type == "none","","."),
+                    ifelse(id.type == "none","",id.type),
+                    ".fa.gz"
                 )
             )
-    }, error = function(e) {
-        warning(
-            "The API 'http://rest.ensembl.org' does not seem to work properly. ", 
-            "Are you connected to the internet? Is the homepage ",
-            "'http://rest.ensembl.org' currently available?", call. = FALSE
-        )
-        return(FALSE)
-    })
-    
-    # construct retrieval query
-    ensembl.qry <-
-        paste0(
-            "ftp://ftp.ensembl.org/pub/current_fasta/",
-            stringr::str_to_lower(new.organism),
-            "/",
-            type,
-            "/",
+        
+        if (!exists.ftp.file(url = ensembl.qry, file.path = ensembl.qry)) {
+            message("Unfortunately no ",type," file could be found for organism '",
+                    organism,
+                    "'. Thus, the download of this organism has been omitted.")
+            return(FALSE) 
+        }
+        
+        if (file.exists(file.path(
+            path,
             paste0(
                 new.organism,
                 ".",
-                json.qry.info$default_coord_system_version,
+                rest_api_status$default_coord_system_version,
                 ".",
                 type,
                 ifelse(id.type == "none","","."),
                 ifelse(id.type == "none","",id.type),
                 ".fa.gz"
             )
-        )
-    
-    if (!exists.ftp.file(url = ensembl.qry, file.path = ensembl.qry)) {
-        message("Unfortunately no ",type," file could be found for organism '",
-                organism,
-                "'. Thus, the download of this organism has been omitted.")
-        return(FALSE) 
-    }
-    
-    if (file.exists(file.path(
-            path,
-            paste0(
+        ))) {
+            message("File ",file.path(
+                path,
+                paste0(
                     new.organism,
                     ".",
-                    json.qry.info$default_coord_system_version,
+                    rest_api_status$default_coord_system_version,
                     ".",
                     type,
                     ifelse(id.type == "none","","."),
                     ifelse(id.type == "none","",id.type),
                     ".fa.gz"
-            )
-    ))) {
-            message("File ",file.path(
+                )
+            )," exists already. Thus, download has been skipped.")
+            return(file.path(
+                path,
+                paste0(
+                    new.organism,
+                    ".",
+                    rest_api_status$default_coord_system_version,
+                    ".",
+                    type,
+                    ifelse(id.type == "none","","."),
+                    ifelse(id.type == "none","",id.type),
+                    ".fa.gz"
+                )
+            ))
+        } else {
+                custom_download(url = ensembl.qry,
+                                destfile = file.path(
+                                    path,
+                                    paste0(
+                                        new.organism,
+                                        ".",
+                                        rest_api_status$default_coord_system_version,
+                                        ".",
+                                        type,
+                                        ifelse(id.type == "none", "", "."),
+                                        ifelse(id.type == "none", "", id.type),
+                                        ".fa.gz"
+                                    )
+                                ),
+                                mode = "wb")
+                
+                return(file.path(
                     path,
                     paste0(
-                            new.organism,
-                            ".",
-                            json.qry.info$default_coord_system_version,
-                            ".",
-                            type,
-                            ifelse(id.type == "none","","."),
-                            ifelse(id.type == "none","",id.type),
-                            ".fa.gz"
+                        new.organism,
+                        ".",
+                        rest_api_status$default_coord_system_version,
+                        ".",
+                        type,
+                        ifelse(id.type == "none", "", "."),
+                        ifelse(id.type == "none", "", id.type),
+                        ".fa.gz"
                     )
-            )," exists already. Thus, download has been skipped.")
-    } else {
-        tryCatch({
-            custom_download(ensembl.qry,
-                            destfile = file.path(
-                                path,
-                                paste0(
-                                    new.organism,
-                                    ".",
-                                    json.qry.info$default_coord_system_version,
-                                    ".",
-                                    type,
-                                    ifelse(id.type == "none", "", "."),
-                                    ifelse(id.type == "none", "", id.type),
-                                    ".fa.gz"
-                                )
-                            ),
-                            mode = "wb")
-        }, error = function(e) {
-            warning(
-                "The FTP site of ENSEMBL 'ftp://ftp.ensembl.org/pub/' does not ",
-                "seem to work properly. Are you connected to the internet? ",
-                "Is the site 'ftp://ftp.ensembl.org/pub/' or ",
-                "'http://rest.ensembl.org' currently available?", call. = FALSE
-            )
-            return(FALSE)
-        })
+                ))
+        }
     }
-    
-    return(file.path(
-        path,
-        paste0(
-            new.organism,
-            ".",
-            json.qry.info$default_coord_system_version,
-            ".",
-            type,
-            ifelse(id.type == "none", "", "."),
-            ifelse(id.type == "none", "", id.type),
-            ".fa.gz"
-        )
-    ))
 }
