9db0384613071e4a3adaf8ea5726b2735a14aa32
HajkD <hgd23@cam.ac.uk>
2018-05-24 18:20:52 +0100

    new status check

--- R/exists.ftp.file.R
+++ R/exists.ftp.file.R
@@ -7,21 +7,26 @@
 #' @noRd 
 #' @import curl
 exists.ftp.file <- function(url, file.path) {
-    tryCatch({
+    
+    test_connection <- curl::curl_fetch_memory(url = url)
+    
+    if (test_connection$status_code == 226) {
         con <-
             curl::curl(url = url,
                        open = "rb",
                        curl::new_handle(dirlistonly = TRUE))
-    }, error = function(e)
-        stop("Something went wrong with the connection to: ", url))
-    ftp.content <-
-        suppressMessages(readr::read_delim(con, 
-                                           delim = "\n", 
-                                           col_names = FALSE))
-    close(con)
-    if (is.element(as.character(basename(file.path)), 
-                   as.character(ftp.content$X1)))
-        return(TRUE)
-    else
+        
+        ftp.content <-
+            suppressMessages(readr::read_delim(con, 
+                                               delim = "\n", 
+                                               col_names = FALSE))
+        close(con)
+        if (is.element(as.character(basename(file.path)), 
+                       as.character(ftp.content$X1)))
+            return(TRUE)
+        else
+            return(FALSE)
+    } else {
         return(FALSE)
+    }
 }
