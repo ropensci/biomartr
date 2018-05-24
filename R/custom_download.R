989a6237c8c0f7d38705f863fff396e506ce8b85
HajkD <hgd23@cam.ac.uk>
2018-05-24 18:20:48 +0100

    new status check

--- R/custom_download.R
+++ R/custom_download.R
@@ -5,35 +5,42 @@
 #' \code{\link[downloader]{download}}
 #' @author Hajk-Georg Drost
 #' @noRd
-custom_download <- function(...) {
+custom_download <- function(url, ...) {
+    
+    if (curl::curl_fetch_memory(url)$status_code == 226) {
+        operating_sys <- Sys.info()[1]
         
-    operating_sys <- Sys.info()[1]
-    
-    if (operating_sys == "Darwin") {
-        downloader::download(
-            ...,
-            method = "curl",
-            extra = "--retry 3",
-            cacheOK = FALSE,
-            quiet = TRUE
-        )
+        if (operating_sys == "Darwin") {
+            downloader::download(
+                url, ...,
+                method = "curl",
+                extra = "--retry 3",
+                cacheOK = FALSE,
+                quiet = TRUE
+            )
+            
+        }
         
-    }
-    
-    if (operating_sys == "Linux") {
-        downloader::download(
-            ...,
-            method = "wget",
-            extra = "--tries 3 --continue",
-            cacheOK = FALSE,
-            quiet = TRUE
+        if (operating_sys == "Linux") {
+            downloader::download(
+                url, ...,
+                method = "wget",
+                extra = "--tries 3 --continue",
+                cacheOK = FALSE,
+                quiet = TRUE
+            )
+        }
+        
+        if (operating_sys == "Windows") {
+            downloader::download(url, ...,
+                                 method = "internal",
+                                 cacheOK = FALSE,
+                                 quiet = TRUE)
+        }
+    } else {
+        warning(
+            "The FTP link: '",url,"' is not available. This might be due to an instable internet connection, a firewall issue, or wrong organism name.", call. = FALSE
         )
-    }
-    
-    if (operating_sys == "Windows") {
-        downloader::download(...,
-                             method = "internal",
-                             cacheOK = FALSE,
-                             quiet = TRUE)
+        return(FALSE)
     }
 }
