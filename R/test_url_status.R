dcfe5972e24b3b576385bc477e6aa2c28c242301
HajkD <hgd23@cam.ac.uk>
2018-05-26 16:17:11 +0100

    add new helper function for url test

---
+++ R/test_url_status.R
@@ -0,0 +1,19 @@
+test_url_status <- function(url, organism) {
+    test_status <- curl::curl_fetch_memory(url)
+    
+    if (test_status$status_code == 200) {
+        json.qry.info <-
+            jsonlite::fromJSON(url)
+        return(json.qry.info)
+    } else {
+        warning(
+            "The url: ",
+            url,
+            " could not be reached. It seems like the organism '",
+            organism,
+            "' does not exist in this database. Could it be that the organism name is misspelled? Thus, download has been omitted.",
+            call. = FALSE
+        )
+        return(FALSE)
+    }
+}
