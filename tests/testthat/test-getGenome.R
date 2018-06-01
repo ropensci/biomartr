669fa6b6b9839af781fd2af2f032232d36bc2d62
HajkD <hgd23@cam.ac.uk>
2018-06-01 18:29:52 +0100

    add new unit tests

--- tests/testthat/test-getGenome.R
+++ tests/testthat/test-getGenome.R
@@ -79,12 +79,68 @@ test_that("The getGenome() interface works properly for NCBI Genbank (including 
     )
 })
 
-test_that("The getGenome() interface works properly for ENSEMBL..",{
+test_that("The getGenome() interface works properly for NCBI Genbank using taxid (including when command is repeated)..", {
+    skip_on_cran()
+    skip_on_travis()
+    # test proper download from genbank
+    read_genome(
+        getGenome(
+            db       = "genbank",
+            organism = "559292",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+    
+    # test proper use of internal referece files when command is repeated
+    read_genome(
+        getGenome(
+            db       = "genbank",
+            organism = "559292",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+})
+
+test_that("The getGenome() interface works properly for NCBI Genbank using accession ids (including when command is repeated)..", {
+    skip_on_cran()
+    skip_on_travis()
+    # test proper download from genbank
+    read_genome(
+        getGenome(
+            db       = "genbank",
+            organism = "GCA_000146045.2",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+    
+    # test proper use of internal referece files when command is repeated
+    read_genome(
+        getGenome(
+            db       = "genbank",
+            organism = "GCA_000146045.2",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+})
+
+test_that("The getGenome() interface works properly for ENSEMBL (including repeating function call)..",{
         
         skip_on_cran()
         skip_on_travis()    
     # test proper download from ENSEMBL
-    Scerevisiae_Genome <-
+        read_genome(
+            getGenome(
+                db       = "ensembl",
+                organism = "Saccharomyces cerevisiae",
+                path     = tempdir()
+            ),
+            format = "fasta"
+        )
+        
         read_genome(
             getGenome(
                 db       = "ensembl",
@@ -94,6 +150,57 @@ test_that("The getGenome() interface works properly for ENSEMBL..",{
             format = "fasta"
         )
 })
+
+test_that("The getGenome() interface works properly for ENSEMBL using taxid (including repeating function call)..",{
+    
+    skip_on_cran()
+    skip_on_travis()    
+    # test proper download from ENSEMBL
+    read_genome(
+        getGenome(
+            db       = "ensembl",
+            organism = "4932",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+    
+    read_genome(
+        getGenome(
+            db       = "ensembl",
+            organism = "4932",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+})
+
+
+test_that("The getGenome() interface works properly for ENSEMBL using accession id (including repeating function call)..",{
+    
+    skip_on_cran()
+    skip_on_travis()    
+    # test proper download from ENSEMBL
+    read_genome(
+        getGenome(
+            db       = "ensembl",
+            organism = "GCA_000146045.2",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+    
+    read_genome(
+        getGenome(
+            db       = "ensembl",
+            organism = "GCA_000146045.2",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+})
+
+
 
 test_that("The getGenome() interface works properly for ENSEMBLGENOMES..", {
     skip_on_cran()
@@ -109,10 +216,19 @@ test_that("The getGenome() interface works properly for ENSEMBLGENOMES..", {
         )
 })
 
-test_that("The getGenome() interface works properly for ENSEMBLGENOMES..", {
+test_that("The getGenome() interface works properly for ENSEMBLGENOMES (including repeating command call)..", {
     skip_on_cran()
     skip_on_travis()
     # test proper use of internal referece files when command is repeated
+        read_genome(
+            getGenome(
+                db       = "ensemblgenomes",
+                organism = "Saccharomyces cerevisiae",
+                path     = tempdir()
+            ),
+            format = "fasta"
+        )
+        
         read_genome(
             getGenome(
                 db       = "ensemblgenomes",
@@ -128,13 +244,67 @@ test_that("The getGenome() error messages work properly for ENSEMBLGENOMES..",
           {
               skip_on_cran()
               skip_on_travis()
-                  getGenome(
+                  expect_equal(getGenome(
                       db       = "ensemblgenomes",
                       organism = "Saccharomyces cerevisi",
                       path     = tempdir()
-                  )
+                  ), FALSE)
               
           })
+
+
+
+test_that("The getGenome() interface works properly for ENSEMBLGENOMES using taxid (including repeating function call)..",{
+    
+    skip_on_cran()
+    skip_on_travis()    
+    # test proper download from ENSEMBLGENOMES
+    read_genome(
+        getGenome(
+            db       = "ensemblgenomes",
+            organism = "4932",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+    
+    read_genome(
+        getGenome(
+            db       = "ensemblgenomes",
+            organism = "4932",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+})
+
+
+test_that("The getGenome() interface works properly for ENSEMBLGENOMES using accession id (including repeating function call)..",{
+    
+    skip_on_cran()
+    skip_on_travis()    
+    # test proper download from ENSEMBLGENOMES
+    read_genome(
+        getGenome(
+            db       = "ensemblgenomes",
+            organism = "GCA_000146045.2",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+    
+    read_genome(
+        getGenome(
+            db       = "ensemblgenomes",
+            organism = "GCA_000146045.2",
+            path     = tempdir()
+        ),
+        format = "fasta"
+    )
+})
+
+
+
 
 test_that("The getGenome() error messages work properly for ENSEMBL..", {
     skip_on_cran()
