376f80ff630ff5de33e649cce5c0324d96dd5bae
HajkD <hgd23@cam.ac.uk>
2018-06-09 14:25:48 +0100

    update unit test

--- tests/testthat/test-getGFF.R
+++ tests/testthat/test-getGFF.R
@@ -1,43 +1,66 @@
 context("Test: getGFF()")
 
-test_that("The getGFF() interface works properly for NCBI RefSeq..",{
+test_that("The getGFF() interface works properly for NCBI RefSeq (repeating command)..",{
     
         skip_on_cran()
         skip_on_travis()
-        Sys.sleep(1)
     # test proper download from refseq
     getGFF( db       = "refseq", 
             organism = "Saccharomyces cerevisiae", 
-            path = file.path("_ncbi_downloads","annotation"))
-})    
+            path = tempdir())
+    
+    getGFF( db       = "refseq", 
+            organism = "Saccharomyces cerevisiae", 
+            path = tempdir())
+})
 
-test_that("The getGFF() interface works properly for NCBI Genbank..",{
+test_that("The getGFF() interface works properly for NCBI RefSeq using taxid (repeating command)..",{
+    
+    skip_on_cran()
+    skip_on_travis()
+    # test proper download from refseq
+    getGFF( db       = "refseq", 
+            organism = "559292", 
+            path = tempdir())
+    
+    getGFF( db       = "refseq", 
+            organism = "559292", 
+            path = tempdir())
+})
+
+test_that("The getGFF() interface works properly for NCBI Genbank (repeating command)..",{
         skip_on_travis()
         skip_on_cran()
-        Sys.sleep(1)
     # test proper download from genbank
     getGFF( db       = "genbank", 
             organism = "Saccharomyces cerevisiae", 
-            path = file.path("_ncbi_downloads","annotation"))
+            path = tempdir())
+    
+    getGFF( db       = "genbank", 
+            organism = "Saccharomyces cerevisiae", 
+            path = tempdir())
 })    
 
 
-# test_that("The getGFF() interface works properly for Ensembl",{
-#         skip_on_cran()
-#         skip_on_travis()
-#         Sys.sleep(1)
-#     # test proper download from Ensembl
-#     getGFF( db       = "ensembl", 
-#             organism = "Saccharomyces cerevisiae", 
-#             path = file.path("ensembl","annotation"))
-# })
-# 
-# test_that("The getGFF() interface works properly for EnsemblGenomes",{
-#         skip_on_cran()
-#         skip_on_travis()
-#         Sys.sleep(1)
-#     # test proper download from EnsemblGenomes
-#     getGFF( db       = "ensemblgenomes", 
-#             organism = "Saccharomyces cerevisiae", 
-#             path = file.path("ensembl","annotation"))
-# })
+test_that("The getGFF() interface works properly for Ensembl (repeating command)",{
+        skip_on_cran()
+        skip_on_travis()
+
+    # test proper download from Ensembl
+    getGFF( db       = "ensembl",
+            organism = "Saccharomyces cerevisiae",
+            path = tempdir())
+    
+    getGFF( db       = "ensembl",
+            organism = "Saccharomyces cerevisiae",
+            path = tempdir())
+})
+
+test_that("The getGFF() interface works properly for EnsemblGenomes",{
+        skip_on_cran()
+        skip_on_travis()
+    # test proper download from EnsemblGenomes
+    getGFF( db       = "ensemblgenomes",
+            organism = "Saccharomyces cerevisiae",
+            path = file.path("ensembl","annotation"))
+})
