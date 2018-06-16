478a453071c9ac392e24d287854804493564a396
HajkD <hgd23@cam.ac.uk>
2018-06-16 14:49:40 +0100

    update unit test

--- tests/testthat/test-getGENOMEREPORT.R
+++ tests/testthat/test-getGENOMEREPORT.R
@@ -1,7 +1,8 @@
 context("Test: getGENOMEREPORT()")
 
 test_that("The getGENOMEREPORT() interface works properly..",{
-    
+    skip_on_cran()
+    skip_on_travis()
     # NCBI GENOMEREPORT Info file retrieval
     getGENOMEREPORT()
 })
