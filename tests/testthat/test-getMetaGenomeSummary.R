3015084af817af6f91193c10dec8c630fa69d25b
HajkD <hgd23@cam.ac.uk>
2018-06-18 17:45:09 +0100

    update unit test

--- tests/testthat/test-getMetaGenomeSummary.R
+++ tests/testthat/test-getMetaGenomeSummary.R
@@ -1,5 +1,7 @@
 context("Test: getMetaGenomeSummary()")
 
 test_that("The getMetaGenomeSummary() interface works properly..",{
+    skip_on_cran()
+    skip_on_travis()
     getMetaGenomeSummary()
 })
