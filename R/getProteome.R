0c9b6e070020ca7b18542c1c45deeb3f5c11b45a
HajkD <hajkdrost@googlemail.com>
2018-12-05 17:55:13 +0000

    adapt to new NCBI md5checksum file format

--- R/getProteome.R
+++ R/getProteome.R
@@ -240,7 +240,7 @@ getProteome <-
                             file_name <- NULL
                             
                             md5_sum <- dplyr::filter(md5_file,
-                                            file_name == paste0("./", paste0(
+                                            file_name == paste0(" ./", paste0(
                                             basename(FoundOrganism$ftp_path),
                                                 "_protein.faa.gz"
                                             )))$md5
