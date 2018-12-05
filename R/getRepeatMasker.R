56b8f0134909c3324483ba5300016be30f1c1cf3
HajkD <hajkdrost@googlemail.com>
2018-12-05 17:55:20 +0000

    adapt to new NCBI md5checksum file format

--- R/getRepeatMasker.R
+++ R/getRepeatMasker.R
@@ -232,7 +232,7 @@ getRepeatMasker <-
                             file_name <- NULL
                             
                             md5_sum <- dplyr::filter(md5_file,
-                                                     file_name == paste0("./", paste0(
+                                                     file_name == paste0(" ./", paste0(
                                                          basename(FoundOrganism$ftp_path),
                                                          "_rm.out.gz"
                                                      )))$md5
