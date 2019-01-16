f320d0a03a8ba7f061bf1d1c8d881d3e1a5ef855
HajkD <hgd23@cam.ac.uk>
2019-01-16 16:40:02 +0000

    update

--- R/tidy_name.R
+++ R/tidy_name.R
@@ -1,16 +1,11 @@
 tidy_name <- function(x) {
     new_spec_names <- unlist(lapply(x, function(y) {
         split_str <- unlist(stringr::str_split(y, "_"))
-        if (y == split_str) {
-            return(y)
-        } else {
             return(paste0(
                 stringr::str_to_upper(stringr::str_sub(split_str[1], 1, 1)),
                 split_str[2],
                 collapse = ""
             ))
-        }
-        
     }))
     
     return(new_spec_names)
