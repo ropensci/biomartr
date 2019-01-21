e24371b57791586c297b67bd5b8c6077da46aecd
HajkD <hgd23@cam.ac.uk>
2019-01-21 18:34:47 +0000

    now clean.retrieval can handle any type of retrieval data not only fasta files

--- R/clean.retrieval.R
+++ R/clean.retrieval.R
@@ -40,11 +40,44 @@ clean.retrieval <- function(x, gunzip = TRUE) {
     if (length(folder_files) == 0)
         stop("Unfortunately, your specified folder '", x, "' does not include any files.", call. = FALSE)
     
+    file_ext <- "[.]*a$"
+        
+    if (any(stringr::str_detect(folder_files, "[.]faa.gz$"))) {
+        seq_type <- "ncbi_protein"
+        file_ext <- "[.]faa$"
+    }
+        
+    if (any(stringr::str_detect(folder_files, "[.]fna.gz$"))) {
+        seq_type <- "ncbi_nucleotide"
+        file_ext <- "[.]fna$"
+    }
+    if (any(stringr::str_detect(folder_files, "[.]gff.gz$"))) {
+        seq_type <- "ncbi_gff"
+        file_ext <- "[.]gff$"
+    }
+    if (any(stringr::str_detect(folder_files, "[.]out.gz$"))) {
+        seq_type <- "ncbi_rm"
+        file_ext <- "[.]out$"
+    }
+    if (any(stringr::str_detect(folder_files, "[.]gff3.gz$"))) {
+        seq_type <- "ensembl_gff3"
+        file_ext <- "[.]gff3$"   
+    }
+    if (any(stringr::str_detect(folder_files, "[.]gtf.gz$"))) {
+        seq_type <- "ensembl_gtf"
+        file_ext <- "[.]gtf$"
+    }
+    if (any(stringr::str_detect(folder_files, "[.]fa.gz$"))) {
+        seq_type <- "ensembl_fasta"
+        file_ext <- "[.]fa$"   
+    }
+    
+    
     # remove doc, md5checksum files, and already unzipped files
     find_doc <- which(stringr::str_detect(folder_files, "doc_"))
     find_md5 <- which(stringr::str_detect(folder_files, "md5checksum"))
     find_documentaion <- which(stringr::str_detect(folder_files, "documentation"))
-    find_unzipped_files <- which(stringr::str_detect(folder_files, "[.]*a$"))
+    find_unzipped_files <- which(stringr::str_detect(folder_files, file_ext))
         
     if (length(c(find_doc, find_md5, find_documentaion, find_unzipped_files)) > 0) {
         folder_files_reduced <- folder_files[-c(find_doc, find_md5, find_documentaion, find_unzipped_files)]
@@ -59,11 +92,14 @@ clean.retrieval <- function(x, gunzip = TRUE) {
     
     input_files_without_appendix <- unlist(lapply(input_files, function(x) return(unlist(stringr::str_split(x, "[.]"))[1])))
     
+    file_ext <- stringr::str_replace(file_ext, "\\$", "") 
+    file_ext <- stringr::str_replace(file_ext, "\\[.]", "")
+        
     if (gunzip)
-        output_files <- paste0(tidy_name(input_files_without_appendix),".fa")
+        output_files <- paste0(tidy_name(input_files_without_appendix), ".", file_ext)
     
     if (!gunzip)
-    output_files <- paste0(tidy_name(input_files_without_appendix),".fa.gz")
+    output_files <- paste0(tidy_name(input_files_without_appendix),".",file_ext,".gz")
     
     if (!all(file.exists(file.path(dirname(x)[1], input_files))))
         stop("Something went wrong during the cleaning process. Some input files seem not to exist.", call. = FALSE)
