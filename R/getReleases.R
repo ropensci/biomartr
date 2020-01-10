17669ad21fe857b5b1d4cba9e6b9160419d6dd69
HajkD <hajkdrost@googlemail.com>
2020-01-10 16:12:31 +0100

    remove ensemblgenomes access

--- R/getReleases.R
+++ R/getReleases.R
@@ -1,28 +1,24 @@
 #' @title Retrieve available database releases or versions 
-#' of ENSEMBL and ENSEMBLGENOMES
+#' of ENSEMBL
 #' @description Retrieve available database releases or versions of 
-#' ENSEMBL and ENSEMBLGENOMES.
+#' ENSEMBL.
 #' @param db a character string specifying the database from which 
 #' available resease versions shall be retrieved:
 #' \itemize{
 #' \item \code{db = "ensembl"}
-#' \item \code{db = "ensemblgenomes"}
 #' }
 #' @author Hajk-Georg Drost
 #' @examples
 #' \dontrun{ 
 #' # retrieve available resease versions of ENSEMBL
 #' getReleases("ensembl")
-#' # retrieve available resease versions of ENSEMBLGENOMES
-#' getReleases("ensemblgenomes")
 #' }
 #' @export
-getReleases <- function(db = "refseq") {
+getReleases <- function(db = "ensembl") {
     
-    if (!is.element(db, c("ensembl", "ensemblgenomes")))
+    if (!is.element(db, c("ensembl")))
         stop(
-            "Please select one of the available databases: 'ensembl'
-            or 'ensemblgenomes'.",
+            "Please select one of the available databases: 'ensembl'.",
             call. = FALSE
         )
     
@@ -33,12 +29,11 @@ getReleases <- function(db = "refseq") {
             jsonlite::fromJSON(
 "http://rest.ensembl.org/info/data/?content-type=application/json")$releases
         }, error = function(e)
-            stop(
+            message(
                 "The API 'http://rest.ensembl.org' does not seem 
-                to work properly. Are you connected to the internet? 
-                Is the homepage 'http://rest.ensembl.org' 
-                currently available?",
-                call. = FALSE
+                to be reachable. Could you please check whether you are connected to the internet? 
+                Is it possible to access the homepage 'http://rest.ensembl.org' 
+                via your browser?"
             ))
         
         message("The current ENSEMBL release is release-", current_release, ".")
@@ -50,17 +45,16 @@ getReleases <- function(db = "refseq") {
         tryCatch({  
             current_release <-
                 jsonlite::fromJSON(
-"http://rest.ensemblgenomes.org/info/data/?content-type=application/json")$releases
+"http://rest.ensembl.org/info/data/?content-type=application/json")$releases
         }, error = function(e)
-            stop(
-                "The API 'http://rest.ensemblgenomes.org' does not seem 
-                to work properly. Are you connected to the internet? 
-                Is the homepage 'http://rest.ensemblgenomes.org' 
-                currently available?",
-                call. = FALSE
-            ))
+          message(
+            "The API 'http://rest.ensembl.org' does not seem 
+                to be reachable. Could you please check whether you are connected to the internet? 
+                Is it possible to access the homepage 'http://rest.ensembl.org' 
+                via your browser?"
+          ))
         
-        message("The current ENSEMBLGENOMES release is release-",
+        message("The current ENSEMBL release is release-",
                 current_release, ".")
         return(paste0("release-", seq_len(current_release)))
     }
