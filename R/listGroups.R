a0a5ee0c5d8e4e43e5177106ba449f7efb75a02a
HajkD <hajkdrost@googlemail.com>
2020-03-18 16:29:38 +0100

    update docu

--- R/listGroups.R
+++ R/listGroups.R
@@ -1,10 +1,13 @@
-#' @title List number of available genomes in each group
+#' @title List number of available genomes in each taxonomic group
 #' @description Users can retrieve the available number of sequenced 
 #' genomes per group. Only available for \code{db = "refseq"} and 
 #' \code{db = "genbank"}.
 #' @param db a character string specifying the database for which genome 
-#' availability shall be checked, e.g. \code{db = "refseq"} and 
-#' \code{db = "genbank"}.
+#' availability shall be checked. Available options are:
+#' \itemize{
+#' \item \code{db = "refseq"} 
+#' \item \code{db = "genbank"}
+#' }
 #' @param kingdom a kingdom specification retrieved by 
 #' \code{\link{getKingdoms}}.
 #' @param details shall all species corresponding to the specified 
