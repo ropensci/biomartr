7fd28741c6561cffd46be2a3b86a063ca0e84191
HajkD <hajkdrost@googlemail.com>
2020-08-08 14:29:00 +0200

    fixing an issue that caused the read_cds() function to fail in `data.table` mode (Many thanks to Clement Kent) #57

--- R/read_cds.R
+++ R/read_cds.R
@@ -48,6 +48,9 @@ read_cds <-
                 call. = FALSE
             )
         
+        if (!file.exists(file))
+            stop("The file path you specified does not seem to exist: '", file,"'.", call. = FALSE)
+        
         geneids <- seqs <- NULL
         
         if (obj.type == "Biostrings") {
@@ -75,20 +78,20 @@ read_cds <-
         }
         
         if (obj.type == "data.table") {
+            geneids <- seqs <- NULL
+            
             tryCatch({
                 cds_file <-
-                    Biostrings::readBStringSet(filepath = file, 
-                                               format = format, ...)
+                    Biostrings::readDNAStringSet(filepath = file, format = format, ...)
                 
                 cds_names <-
-                    as.vector(unlist(lapply(cds_file@ranges@NAMES, function(x) {
+                    as.vector(unlist(sapply(cds_file@ranges@NAMES, function(x) {
                         return(strsplit(x, " ")[[1]][1])
                     })))
                 
                 cds.dt <-
                     data.table::data.table(geneids = cds_names ,
-                                           seqs = 
-                                               tolower(as.character(cds_file)))
+                                           seqs = tolower(as.character(cds_file)))
                 
                 
                 data.table::setkey(cds.dt, geneids)
@@ -98,42 +101,113 @@ read_cds <-
                         return((nchar(x) %% 3) == 0)
                     }
                 
-                all_triplets <- cds.dt[, mod3(seqs)]
+                all_triplets <- as.logical(cds.dt[ , mod3(seqs)])
                 
-                if (!all(all_triplets)) {
-                    warning(
-                        paste0(
-                            "There have been ",
-                            length(which(!all_triplets)),
-                            " genes found that cannot be divided by 3. 
-                            To delete these sequences please specify the 
-                            'delete_corrupt = TRUE' argument."
-                        )
-                    )
-                }
+                n_seqs <- nrow(cds.dt)
+                
             }, error = function(e) {
                 stop(
-                    paste0(
-                        "File ",
-                        file,
-                        " could not be read properly. \n",
-                        "Please make sure that ",
-                        file,
-                        " contains only CDS sequences and is in ",
-                        format,
-                        " format."
-                    ),
-                    call. = FALSE
+                    "File ",
+                    file,
+                    " could not be read properly.",
+                    "\n",
+                    "Please make sure that ",
+                    file,
+                    " contains only CDS sequences and is in ",
+                    format,
+                    " format."
                 )
             })
             
             if (!all(all_triplets)) {
-                if (delete_corrupt)
+                message(
+                    "There seem to be ",
+                    length(which(!all_triplets)),
+                    " coding sequences in your input dataset which cannot be properly divided in base triplets, because their sequence length cannot be divided by 3."
+                )
+                corrupted_file <-
+                    paste0(basename(file), "_corrupted_cds_seqs.fasta")
+                
+                message(
+                    "A fasta file storing all corrupted coding sequences for inspection was generated and stored at '",
+                    file.path(getwd(), corrupted_file),
+                    "'."
+                )
+                message("\n")
+                corrupted_seqs <- as.data.frame(cds.dt[which(!all_triplets)])
+                seq_vector <- corrupted_seqs$seqs
+                names(seq_vector) <- corrupted_seqs$geneids
+                corrupted_seqs_biostrings <- Biostrings::DNAStringSet(seq_vector, use.names = TRUE)                                       
+                Biostrings::writeXStringSet(corrupted_seqs_biostrings, filepath = corrupted_file)
+                
+                if (delete_corrupt) {
+                    message(
+                        "You chose option 'delete_corrupt = TRUE', thus corrupted coding sequences were removed.",
+                        "If after consulting the file '",
+                        corrupted_file,
+                        "' you still wish to retain all coding sequences please specify the argument 'delete_corrupt = FALSE'."
+                    )
+                    message("\n")
                     return(cds.dt[-which(!all_triplets) , list(geneids, seqs)])
+                }
                 
-                if (!delete_corrupt)
+                if (!delete_corrupt) {
+                    message(
+                        "You chose option 'delete_corrupt = FALSE', thus corrupted coding sequences were retained for subsequent analyses.")
+                    message(
+                        "The following modifications were made to the CDS sequences that were not divisible by 3:")
+                    message(
+                        "- If the sequence had 1 residue nucleotide then the last nucleotide of the sequence was removed.")
+                    message(
+                        "- If the sequence had 2 residue nucleotides then the last two nucleotides of the sequence were removed.")
+                    message(
+                        "If after consulting the file '",
+                        corrupted_file,
+                        "' you wish to remove all corrupted coding sequences please specify the argument 'delete_corrupt = TRUE'."
+                    )
+                    
+                    mod3_residue_1 <-
+                        function(x) {
+                            return((nchar(x) %% 3) == 1)
+                        }
+                    
+                    mod3_residue_2 <-
+                        function(x) {
+                            return((nchar(x) %% 3) == 2)
+                        }
+                    
+                    residue_1 <- cds.dt[ , mod3_residue_1(seqs)]
+                    residue_2 <- cds.dt[ , mod3_residue_2(seqs)]
+                    
+                    residue_1_seqs <- as.character(cds.dt[which(residue_1) , seqs])
+                    residue_2_seqs <- as.character(cds.dt[which(residue_2) , seqs])
+                    
+                    residue_1_seqs_vec <- as.character(sapply(residue_1_seqs, function(x) {
+                        stringr::str_sub(x, 1, nchar(x) - 1)
+                    }))
+                    
+                    residue_2_seqs_vec <- as.character(sapply(residue_2_seqs, function(x) {
+                        stringr::str_sub(x, 1, nchar(x) - 2)
+                    }))
+                    
+                    cds.dt[which(residue_1) , seqs := residue_1_seqs_vec]
+                    cds.dt[which(residue_2) , seqs := residue_2_seqs_vec]
+                    
+                    all_triplets_new <- cds.dt[ , mod3(seqs)]
+                    
+                    if (any(!all_triplets_new)) {
+                        stop("Something went wring during the trimming process. Not all sequences were trimmed properly.", call. = FALSE)
+                    } else {
+                        message("All corrupted CDS were trimmed.")
+                    }
+                    
+                    n_seqs_new <- nrow(cds.dt)
+                    
+                    if(!(n_seqs == n_seqs_new))
+                       
+                         stop("After trimming corrupted CDS some sequences seem to be lost. Please check what might have gone wrong with the sequence trimming.", call. = FALSE)
                     return(cds.dt)
-                
+                }
             } else {
                 return(cds.dt)
             }
