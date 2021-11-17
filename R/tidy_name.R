9a3019dfad1d70618a39e528b3daad7071611aff
HajkD <hajkdrost@googlemail.com>
2021-11-17 17:09:09 +0100

    update naming convention for tidy names

--- R/tidy_name.R
+++ R/tidy_name.R
@@ -3,7 +3,9 @@ tidy_name <- function(x) {
         split_str <- unlist(stringr::str_split(y, "_"))
             return(paste0(
                 stringr::str_to_upper(stringr::str_sub(split_str[1], 1, 1)),
-                split_str[2],
+                stringr::str_sub(split_str[1], 2, nchar(split_str[1])),
+                stringr::str_to_upper(stringr::str_sub(split_str[2],1,1)),
+                stringr::str_sub(split_str[2], 2, nchar(split_str[2])),
                 collapse = ""
             ))
     }))
--- R/tidy_name2.R
+++ R/tidy_name2.R
@@ -2,9 +2,11 @@ tidy_name2 <- function(x) {
     new_spec_names <- unlist(lapply(x, function(y) {
         split_str <- unlist(stringr::str_split(y, " "))
         return(paste0(
-            stringr::str_to_upper(stringr::str_sub(split_str[1], 1, 1)),
-            split_str[2],
-            collapse = ""
+          stringr::str_to_upper(stringr::str_sub(split_str[1], 1, 1)),
+          stringr::str_sub(split_str[1], 2, nchar(split_str[1])),
+          stringr::str_to_upper(stringr::str_sub(split_str[2],1,1)),
+          stringr::str_sub(split_str[2], 2, nchar(split_str[2])),
+          collapse = ""
         ))
     }))
     
