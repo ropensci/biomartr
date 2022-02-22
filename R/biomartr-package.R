e9337c99f1010f175dacdcdfa5ce40dd18680247
HajkD <hajkdrost@googlemail.com>
2022-02-22 13:27:27 +0100

    update NAMESPACES

--- R/biomartr-package.R
+++ R/biomartr-package.R
@@ -32,6 +32,17 @@
 #' @name biomartr-package
 #' @aliases biomartr
 #' @docType package
-#' @author Hajk-Georg Drost \email{hgd23@cam.ac.uk}
+#' @author Hajk-Georg Drost \email{hajk-georg.drost@tuebingen.mpg.de}
 #' @keywords package
+## usethis namespace: start
+#' @importFrom data.table data.table
+#' @importFrom data.table :=
+#' @importFrom data.table .SD
+#' @importFrom data.table .BY
+#' @importFrom data.table .N
+#' @importFrom data.table .I
+#' @importFrom data.table .GRP
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .EACHI
+## usethis namespace: end
 NULL
