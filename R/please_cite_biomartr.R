45baeba4eaeeb3f8bfd5fea703e986c70247275f
Hajk-Georg Drost <hdrost@a6-hajk-mm.eb.local>
2023-08-20 19:05:44 +0200

    add new function

---
+++ R/please_cite_biomartr.R
@@ -0,0 +1,7 @@
+please_cite_biomartr <- function(mute_citation = FALSE) {
+  if (!mute_citation) {
+    cat("\n")
+    cat("\n")
+    cat("Please cite: Drost HG, Paszkowski J. Biomartr: genomic data retrieval with R. Bioinformatics (2017) 33(8): 1216-1217. doi:10.1093/bioinformatics/btw821.")
+  }
+}
