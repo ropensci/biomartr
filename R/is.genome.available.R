b5f2f1abc918ce3a24ca4b04e304cfc0336fd564
Hajk-Georg Drost <hdrost@a6-hajk-mm.eb.local>
2023-08-20 19:04:52 +0200

    add new arguments to skip_bacteria download by default and add citation reference

--- R/is.genome.available.R
+++ R/is.genome.available.R
@@ -15,6 +15,10 @@
 #' \item by \code{database specific accession identifier}: e.g. \code{organism = "GCF_000001405.37"} (= NCBI RefSeq identifier for \code{Homo sapiens})
 #' \item by \code{taxonomic identifier from NCBI Taxonomy}: e.g. \code{organism = "9606"} (= taxid of \code{Homo sapiens})
 #' }
+#' @param skip_bacteria Due to its enormous dataset size (> 700MB as of July 2023), 
+#' the bacterial summary file will not be loaded by default anymore. If users
+#' wish to gain insights for the bacterial kingdom they needs to actively specify \code{skip_bacteria = FALSE}. When \code{skip_bacteria = FALSE} is set then the 
+#' bacterial summary file will be downloaded.   
 #' @param details a logical value specifying whether or not details on genome
 #' size, kingdom, etc. shall be printed to the console intead of a
 #' boolean value.
@@ -47,6 +51,7 @@ is.genome.available <-
     function(
              db = "refseq",
              organism,
+             skip_bacteria = "TRUE",
              details = FALSE
              ) {
 
@@ -59,7 +64,7 @@ is.genome.available <-
             )
 
         if (is.element(db, c("refseq", "genbank"))) {
-          return(is.genome.available.refseq.genbank(db, organism, details))
+          return(is.genome.available.refseq.genbank(db = db, organism = organism, details = details))
         }
 
         if (db == "ensembl") {
@@ -247,129 +252,11 @@ is.genome.available.refseq.genbank <- function(db = "refseq",
                                                organism,
                                                skip_bacteria = TRUE,
                                                details = FALSE) {
-  # if AssemblyFilesAllKingdoms.txt file was already
-  # generated/downloaded then use the local version
-  # stored in temp()
-  if (file.exists(file.path(
-    tempdir(),
-    paste0("AssemblyFilesAllKingdoms_", db, ".txt")
-  )) & skip_bacteria) {
-    suppressWarnings(
-      AssemblyFilesAllKingdoms <-
-        readr::read_tsv(
-          file.path(
-            tempdir(),
-            paste0("AssemblyFilesAllKingdoms_", db, ".txt")
-          ),
-          comment = "#",
-          col_names = c(
-            "assembly_accession",
-            "bioproject",
-            "biosample",
-            "wgs_master",
-            "refseq_category",
-            "taxid",
-            "species_taxid",
-            "organism_name",
-            "infraspecific_name",
-            "isolate",
-            "version_status",
-            "assembly_level",
-            "release_type",
-            "genome_rep",
-            "seq_rel_date",
-            "asm_name",
-            "submitter",
-            "gbrs_paired_asm",
-            "paired_asm_comp",
-            "ftp_path",
-            "excluded_from_refseq",
-            "relation_to_type_material",
-            "asm_not_live_date",
-            "assembly_type",
-            "group",
-            "genome_size",
-            "genome_size_ungapped",
-            "gc_percent",
-            "replicon_count",
-            "scaffold_count",
-            "contig_count",
-            "annotation_provider",
-            "annotation_name",
-            "annotation_date",
-            "total_gene_count",
-            "protein_coding_gene_count",
-            "non_coding_gene_count",
-            "pubmed_id"
-          ),
-          col_types = readr::cols(
-            assembly_accession = readr::col_character(),
-            bioproject = readr::col_character(),
-            biosample = readr::col_character(),
-            wgs_master = readr::col_character(),
-            refseq_category = readr::col_character(),
-            taxid = readr::col_integer(),
-            species_taxid = readr::col_integer(),
-            organism_name = readr::col_character(),
-            infraspecific_name = readr::col_character(),
-            isolate = readr::col_character(),
-            version_status = readr::col_character(),
-            assembly_level = readr::col_character(),
-            release_type = readr::col_character(),
-            genome_rep = readr::col_character(),
-            seq_rel_date = readr::col_date(),
-            asm_name = readr::col_character(),
-            submitter = readr::col_character(),
-            gbrs_paired_asm = readr::col_character(),
-            paired_asm_comp = readr::col_character(),
-            ftp_path = readr::col_character(),
-            excluded_from_refseq = readr::col_character(),
-            relation_to_type_material = readr::col_character(),
-            asm_not_live_date = readr::col_character(),
-            assembly_type = readr::col_character(),
-            group = readr::col_character(),
-            genome_size = readr::col_integer(),
-            genome_size_ungapped = readr::col_integer(),
-            gc_percent = readr::col_double(),
-            replicon_count = readr::col_integer(),
-            scaffold_count = readr::col_integer(),
-            contig_count = readr::col_integer(),
-            annotation_provider = readr::col_character(),
-            annotation_name = readr::col_character(),
-            annotation_date = readr::col_date(format = "%m/%d/%y"),
-            total_gene_count = readr::col_integer(),
-            protein_coding_gene_count = readr::col_integer(),
-            non_coding_gene_count = readr::col_integer(),
-            pubmed_id = readr::col_character()
-          )
-        )
-    )
-  } else {
-    # otherwise download all assembly_summary.txt files for all
-    # kingdoms and store the AssemblyFilesAllKingdoms.txt
-    # file locally
-    # retrieve the assembly_summary.txt files for all kingdoms
-    kgdoms <- getKingdoms(db = db)
-    storeAssemblyFiles <- vector("list", length(kgdoms))
-
-    for (i in seq_along(kgdoms)) {
-      storeAssemblyFiles[i] <-
-        list(getSummaryFile(db = db, kingdom = kgdoms[i]))
-    }
-
-    AssemblyFilesAllKingdoms <-
-      dplyr::bind_rows(storeAssemblyFiles)
-
-    readr::write_tsv(AssemblyFilesAllKingdoms,
-                     file.path(
-                       tempdir(),
-                       paste0("AssemblyFilesAllKingdoms_",
-                              db, ".txt")
-                     ))
-  }
 
   organism_name <- assembly_accession <- taxid <- NULL
 
+  AssemblyFilesAllKingdoms <- getKingdomAssemblySummary(db = db, skip_bacteria = skip_bacteria)
+    
   orgs <-
     stringr::str_replace_all(AssemblyFilesAllKingdoms$organism_name,
                              "\\(", "")
