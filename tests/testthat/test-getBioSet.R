f3ffaee3855d6b65b8606e3a8d83108551b86b18
Hajk-Georg Drost <hdrost@a6-hajk-mm.eb.local>
2023-10-04 16:40:22 +0200

    update unit test

--- tests/testthat/test-getBioSet.R
+++ tests/testthat/test-getBioSet.R
@@ -1,6 +1,8 @@
 context("Test: getBioSet()")
 
 test_that("The getGFFSet() interface works properly for ensembl (repeating command)..",{
+  skip_on_cran()
+  skip_on_travis()
   path <- file.path(tempdir(), "GFF_set")
   out1 <- getGFFSet("ensembl",
                     organisms = c("Saccharomyces cerevisiae", "Escherichia coli"),
